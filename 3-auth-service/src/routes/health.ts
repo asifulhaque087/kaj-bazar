d7e27df316cdc9655661de5903eb9a8f86e39fc4
asifulhaque087 <asifulhaque087@gmail.com>
2025-06-08 13:48:43 +0600

    test: test deploy

--- 3-auth-service/src/routes/health.ts
+++ 3-auth-service/src/routes/health.ts
@@ -4,6 +4,7 @@ import express, { Router } from 'express';
 const router: Router = express.Router();
 
 export function healthRoutes(): Router {
+  console.log("from health check")
   router.get('/auth-health', health);
 
   return router;
