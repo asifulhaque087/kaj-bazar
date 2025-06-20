3cc512f82e481bb7c46885336f72401391bac537
asifulhaque087 <asifulhaque087@gmail.com>
2025-06-20 14:26:43 +0600

    feat: add info modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
-import { FC, ReactElement, useEffect } from 'react';
+import { FC, ReactElement, useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import AppRouter from './AppRoutes';
 import useBeforeWindowUnload from './shared/hooks/useBeforeWindowUnload';
 import { socketService } from './sockets/socket.service';
+import InfoModal from './features/info-modal';
 
 const App: FC = (): ReactElement => {
+  const [isInfoModal, setIsInfoModal] = useState(true);
+
   useBeforeWindowUnload();
 
   useEffect(() => {
@@ -16,6 +19,17 @@ const App: FC = (): ReactElement => {
   return (
     <>
       <BrowserRouter>
+        {isInfoModal && (
+          // <ReviewModal type="buyer-review" order={order} onClose={() => setOrderReviewModal({ ...orderReviewModal, buyerReview: false })} />
+
+          <InfoModal
+            onClose={() => {
+              console.log('hello');
+              setIsInfoModal(false);
+            }}
+          />
+        )}
+
         <div className="w-screen min-h-screen flex flex-col relative">
           <AppRouter />
           <ToastContainer />
