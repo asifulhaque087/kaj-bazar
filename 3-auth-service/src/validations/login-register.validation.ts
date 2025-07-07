61ad4ddea910ead0d89c188674ad3ba554699e0e
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-07 16:20:56 +0600

    feat: add login validation

--- 3-auth-service/src/validations/login-register.validation.ts
+++ 3-auth-service/src/validations/login-register.validation.ts
@@ -7,6 +7,12 @@ import { insertAuthSchema } from "@src/drizzle/schema";
 
 export const signUpValidation = insertAuthSchema;
 
+export const loginValidation = insertAuthSchema.pick({
+  username: true,
+  email: true,
+  password: true,
+});
+
 // export type registerUser = z.infer<typeof registerUserSchema>;
 
 // export const loginUserValidation = insertUserSchema.omit({
