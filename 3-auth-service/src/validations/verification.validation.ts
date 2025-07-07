4d3f9f7a0c6cb5f1b5b6b4dc47e41575b86fa86c
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-07 16:22:00 +0600

    feat: add verification validation

---
+++ 3-auth-service/src/validations/verification.validation.ts
@@ -0,0 +1,11 @@
+// ** Third party imports
+import { z } from "zod";
+
+export const tokenSchema = z.object({
+  token: z.string().min(3, { message: "Title must be at least 3 characters" }),
+});
+
+export const resendEmailSchema = z.object({
+  email: z.string().min(1, { message: "Email is required" }).email(),
+  userId: z.number(),
+});
