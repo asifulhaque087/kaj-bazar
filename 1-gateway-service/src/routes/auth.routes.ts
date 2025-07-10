687b0a6bae0e21f8ad53ccae1e3734037786c9d3
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-10 17:33:47 +0600

    feat: update auth routes

--- 1-gateway-service/src/routes/auth.routes.ts
+++ 1-gateway-service/src/routes/auth.routes.ts
@@ -1,46 +1,97 @@
-// import type { SessionData } from "@src/app";
-import { authService } from "@src/axios-services/auth.axios";
+import { AxiosService } from "@src/axios-services/axios";
+import { config } from "@src/config";
+import { apiMiddleware } from "@src/middlewares/api.middleware";
+import { verifyJwtToken } from "@src/middlewares/verify-jwt.middleware";
 import type { AxiosResponse } from "axios";
+import { AxiosError } from "axios";
 import { Hono } from "hono";
 import type { Session } from "hono-sessions";
+import type { ContentfulStatusCode } from "hono/utils/http-status";
 
 type SessionData = {
   jwt?: string;
   userId?: string;
 };
 
-// type JwtPayload = {
-//   userId: string;
-//   email: string;
-// };
-
 type AppVariables = {
   session: Session<SessionData>;
-  //   jwtPayload?: JwtPayload;
+  publicAxios: AxiosService;
 };
 
+// ** Create Hono App
 const authRouter = new Hono<{ Variables: AppVariables }>();
 
-authRouter.post("/auth/signup", async (c) => {
-  const body = await c.req.parseBody();
+// ** Create APi services
+authRouter.use(apiMiddleware(`${config.AUTH_BASE_URL}/api/v1/auth`, "auth"));
 
-  const response: AxiosResponse = await authService.signUp(body);
+// ** Signup Route Handler
+authRouter.post(
+  "/auth/signup",
 
-  const session = c.get("session");
-  session.set("jwt", response.data.token);
+  async (c) => {
+    const body = await c.req.json();
 
-  return c.json({
-    message: response.data.message,
-    user: response.data.user,
-  });
-});
+    try {
+      // request to auth service
+      const publicAxios = c.get("publicAxios");
+      const response: AxiosResponse = await publicAxios.axios.post(
+        "/signup",
+        body
+      );
+
+      // set jwt
+      const session = c.get("session");
+      session.set("jwt", response.data.token);
+
+      // response data
+      return c.json({
+        message: response.data.message,
+        user: response.data.user,
+      });
+    } catch (error) {
+      if (!(error instanceof AxiosError && error.response))
+        throw new Error("Server Error");
+
+      return c.json(
+        error.response.data,
+        error.response.status as ContentfulStatusCode
+      );
+    }
+  }
+);
 
 authRouter.post("/auth/signin", async (c) => {
-  return c.json({});
+  const body = await c.req.json();
+
+  try {
+    // forward req to auth service
+    const publicAxios = c.get("publicAxios");
+    const response: AxiosResponse = await publicAxios.axios.post(
+      "/signin",
+      body
+    );
+
+    // set jwt to session
+    const session = c.get("session");
+    session.set("jwt", response.data.token);
+
+    // return response
+    return c.json(response.data);
+  } catch (error) {
+    // return errors
+
+    if (!(error instanceof AxiosError && error.response))
+      throw new Error("Server Error");
+
+    return c.json(
+      error.response.data,
+      error.response.status as ContentfulStatusCode
+    );
+  }
 });
 
-authRouter.post("/auth/signout", async (c) => {
-  return c.json({});
+authRouter.post("/auth/signout", verifyJwtToken, async (c) => {
+  return c.json({ token: "bearerToken" });
 });
 
 authRouter.put("/auth/verify-email", async (c) => {
