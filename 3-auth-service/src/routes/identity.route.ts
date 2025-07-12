3b70363ed0f100d60776769b3408133d5d9b0528
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-12 10:23:23 +0600

    feat: add refresh token functionality

--- 3-auth-service/src/routes/identity.route.ts
+++ 3-auth-service/src/routes/identity.route.ts
@@ -1,16 +1,42 @@
 // ** Third party imports
+import { BadRequestError, NotFoundError } from "@fvoid/shared-lib";
+import { config } from "@src/config";
+import { db } from "@src/drizzle/db";
+import { AuthTable } from "@src/drizzle/schema";
+import { verifyClientToken } from "@src/middlewares/verfiyClientToken.middleware";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
+import { sign } from "hono/jwt";
 
 const identityRouter = new Hono();
 
-// **
-identityRouter.get("/refresh-token/:username", async (c) => {
-  return c.json({ message: "I am from refresh token" });
+// ** Refresh token
+identityRouter.get("/refresh-token/:username", verifyClientToken, async (c) => {
+  // get current user email and find user
+  const user = c.get("user");
+
+  const isUser = await db.query.AuthTable.findFirst({
+    where: eq(AuthTable.email, user.email),
+  });
+
+  if (!isUser) throw new BadRequestError("Invalid token");
+
+  // generate jwt
+  const payload = {
+    id: isUser?.id,
+    email: isUser.email,
+    username: isUser.username,
+    exp: Math.floor(Date.now() / 1000) + 24 * 7 * 3600, // This will set the expiry to 7 days from now
+  };
+
+  const userJWT = await sign(payload, config.JWT_TOKEN);
+
+  // return response
+  return c.json({ message: "Refresh token", user: isUser, token: userJWT });
 });
 
-// **
-identityRouter.get("/currentuser", async (c) => {
-  return c.json({ message: "I am from current user" });
+// ** Get Current User
+identityRouter.get("/currentuser", verifyClientToken, async (c) => {
 });
 
 export default identityRouter;
