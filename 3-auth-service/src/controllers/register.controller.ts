3cd2d5b0d7ab93ce174e4b999a7a79a8eff9b09d
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-15 10:38:37 +0600

    feat: add register controller

---
+++ 3-auth-service/src/controllers/register.controller.ts
@@ -0,0 +1,113 @@
+// ** Third Party Imports
+
+import type { UploadApiResponse } from "cloudinary";
+import jwt from "jsonwebtoken";
+import { eq, or } from "drizzle-orm";
+import type { Request, Response } from "express";
+import * as crypto from "node:crypto";
+
+// ** Local Imports
+
+import { BadRequestError, uploads } from "@fvoid/shared-lib";
+import { config } from "@src/config";
+import { db } from "@src/drizzle/db";
+import { AuthTable } from "@src/drizzle/schema";
+import { hashPassword } from "@src/utils/hashing.util";
+import type { UserRegistrationInput } from "@src/validations/login-register.validation";
+import { SendEmailPublisher } from "@src/events/publishers/send-email-publisher";
+import { mqWrapper } from "@src/rabbitmq-wrapper";
+
+const register = async (req: Request, res: Response) => {
+  const {
+    username,
+    email,
+    password,
+    country,
+    profilePicture,
+    // browserName,
+    // deviceType,
+  } = req.body as UserRegistrationInput;
+
+  // Check if user exists by username or email
+  const isUser = await db
+    .select()
+    .from(AuthTable)
+    .where(or(eq(AuthTable.username, username), eq(AuthTable.email, email)))
+    .limit(1)
+    .then((res) => res[0]);
+
+  if (isUser) throw new BadRequestError("User already exits");
+
+  // Upload profile picture
+  const profilePublicId = crypto.randomUUID();
+  let uploadResult: UploadApiResponse;
+  try {
+    uploadResult = (await uploads(
+      profilePicture,
+      `${profilePublicId}`,
+      true,
+      true
+    )) as UploadApiResponse;
+  } catch (error) {
+    console.error("Upload error:", error);
+    throw new Error("File upload error. Try again.");
+  }
+
+  if (!uploadResult.public_id) throw new Error("File upload error. Try again.");
+
+  // Generate email verification token
+  const randomCharacters = crypto.randomBytes(20).toString("hex");
+
+  // hash the password
+  const hashedPassword = await hashPassword(password);
+
+  // Prepare auth data
+  const authData = {
+    username,
+    email,
+    profilePublicId,
+    password: hashedPassword,
+    country,
+    profilePicture: uploadResult?.secure_url,
+    emailVerificationToken: randomCharacters,
+    // browserName,
+    // deviceType,
+  };
+
+  // Create auth user in database
+  const result = await db
+    .insert(AuthTable)
+    .values(authData)
+    .$returningId()
+    .then((res) => res[0]);
+
+  // Send email verification message to queue
+  const verificationLink = `${config.CLIENT_URL}/confirm_email?v_token=${authData.emailVerificationToken}`;
+
+  new SendEmailPublisher(mqWrapper.channel).publish({
+    receiverEmail: authData.email!,
+    verifyLink: verificationLink,
+    template: "verifyEmail",
+    username: authData.username,
+  });
+
+  // Sign JWT token
+  const payload = {
+    id: result?.id,
+    email: authData.email,
+    username: authData.username,
+    // exp: Math.floor(Date.now() / 1000) + 60 * 1, // 1 min
+    exp: Math.floor(Date.now() / 1000) + 24 * 7 * 3600, // This will set the expiry to 7 days from now
+  };
+
+  const userJWT = jwt.sign(payload, config.JWT_TOKEN);
+
+  // Respond to client
+  res.json({
+    message: "User created successfully",
+    user: { id: result?.id, ...authData },
+    token: userJWT,
+  });
+};
+
+export default register;
