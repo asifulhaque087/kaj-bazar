621292dbaf34261083ca4f98142d36409d82c2bf
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-15 12:41:45 +0600

    feat: add refresh token controller

---
+++ 1-gateway-service/src/controllers/auth/refresh-token.controller.ts
@@ -0,0 +1,23 @@
+import type { Request, Response } from "express";
+import { type AxiosResponse, AxiosError } from "axios"; // Import AxiosResponse and AxiosError
+
+const refreshToken = async (req: Request, res: Response) => {
+  const body = req.body;
+  const { token } = req.params;
+
+  try {
+    const apiService = req.protectedAxios!;
+    const response: AxiosResponse = await apiService.axios.get(
+      `/refresh-token`
+    );
+    // Send response data
+    return res.json(response.data);
+  } catch (error) {
+    if (!(error instanceof AxiosError && error.response))
+      throw new Error("Server Error");
+
+    return res.status(error.response.status).json(error.response.data);
+  }
+};
+
+export default refreshToken;
