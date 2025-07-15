bbe9f060a48b27a401e707e3edd0b2454b5dc1de
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-15 10:38:02 +0600

    feat: add forgot password controller

---
+++ 3-auth-service/src/controllers/forgot-password.controller.ts
@@ -0,0 +1,55 @@
+// Third Party Imports
+
+import { BadRequestError } from "@fvoid/shared-lib";
+import { eq } from "drizzle-orm";
+import type { Request, Response } from "express";
+import * as crypto from "node:crypto";
+
+// Local Imports
+
+import { config } from "@src/config";
+import { db } from "@src/drizzle/db";
+import { AuthTable } from "@src/drizzle/schema";
+import { SendEmailPublisher } from "@src/events/publishers/send-email-publisher";
+import { mqWrapper } from "@src/rabbitmq-wrapper";
+import type { ForgotPasswordInput } from "@src/validations/password.validation";
+
+const forgotPassword = async (req: Request, res: Response) => {
+  // extract data
+  const { email } = req.body as ForgotPasswordInput;
+
+  // find user by email
+  const isUser = await db
+    .select()
+    .from(AuthTable)
+    .where(eq(AuthTable.email, email))
+    .limit(1)
+    .then((res) => res[0]);
+
+  if (!isUser) throw new BadRequestError("Invalid credentials");
+
+  // generate new token and update user
+  const randomCharacters = crypto.randomBytes(20).toString("hex");
+  const date: Date = new Date();
+  date.setHours(date.getHours() + 1);
+
+  await db
+    .update(AuthTable)
+    .set({ passwordResetToken: randomCharacters, passwordResetExpires: date })
+    .where(eq(AuthTable.id, isUser.id));
+
+  // send email
+  const passwordResetLink = `${config.CLIENT_URL}/reset_password?token=${randomCharacters}`;
+
+  new SendEmailPublisher(mqWrapper.channel).publish({
+    receiverEmail: email!,
+    resetLink: passwordResetLink,
+    username: isUser.username,
+    template: "forgotPassword",
+  });
+
+  // send response
+  res.json({ message: "Reset password link sent" });
+};
+
+export default forgotPassword;
