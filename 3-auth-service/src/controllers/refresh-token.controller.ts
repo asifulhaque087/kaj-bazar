6032695c8145910fd94174f398af4ffb802196a8
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-15 10:38:25 +0600

    feat: add refresh token controller

---
+++ 3-auth-service/src/controllers/refresh-token.controller.ts
@@ -0,0 +1,40 @@
+// ** Third Party Imports
+
+import { BadRequestError, NotAuthorizedError } from "@fvoid/shared-lib";
+import { eq } from "drizzle-orm";
+import type { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+// ** Local Imports
+
+import { config } from "@src/config";
+import { db } from "@src/drizzle/db";
+import { AuthTable } from "@src/drizzle/schema";
+
+const refreshToken = async (req: Request, res: Response) => {
+  // get current user email and find user
+  const user = req.user;
+
+  if (!user) throw new NotAuthorizedError();
+
+  const isUser = await db.query.AuthTable.findFirst({
+    where: eq(AuthTable.email, user.email),
+  });
+
+  if (!isUser) throw new BadRequestError("Invalid token");
+
+  // generate jwt
+  const payload = {
+    id: isUser?.id,
+    email: isUser.email,
+    username: isUser.username,
+    exp: Math.floor(Date.now() / 1000) + 24 * 7 * 3600, // This will set the expiry to 7 days from now
+  };
+
+  const userJWT = jwt.sign(payload, config.JWT_TOKEN);
+
+  // return response
+  res.json({ message: "Refresh token", user: isUser, token: userJWT });
+};
+
+export default refreshToken;
