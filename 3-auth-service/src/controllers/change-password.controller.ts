7c128b490f92645be877f8b23bf8cb5cda2cab52
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-15 10:37:17 +0600

    feat: add change password controller

---
+++ 3-auth-service/src/controllers/change-password.controller.ts
@@ -0,0 +1,55 @@
+// ** Third Party Imports
+import type { ChangePasswordInput } from "@src/validations/password.validation";
+import { eq } from "drizzle-orm";
+import type { Request, Response } from "express";
+
+// ** Local Imports
+import { NotAuthorizedError, NotFoundError } from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import { AuthTable } from "@src/drizzle/schema";
+import { SendEmailPublisher } from "@src/events/publishers/send-email-publisher";
+import { mqWrapper } from "@src/rabbitmq-wrapper";
+import { hashPassword } from "@src/utils/hashing.util";
+
+const changePassword = async (req: Request, res: Response) => {
+  // extract data
+  const { password } = req.body as ChangePasswordInput;
+
+  // get current user email and find user
+  const user = req.user;
+
+  if (!user) throw new NotAuthorizedError();
+
+  const isUser = await db
+    .select()
+    .from(AuthTable)
+    .where(eq(AuthTable.email, user.email))
+    .limit(1)
+    .then((res) => res[0]);
+
+  if (!isUser) throw new NotFoundError();
+
+  // hash password and update user
+  const hashedPassword = await hashPassword(password);
+
+  await db
+    .update(AuthTable)
+    .set({
+      password: hashedPassword,
+      passwordResetToken: null,
+      passwordResetExpires: null,
+    })
+    .where(eq(AuthTable.id, isUser.id));
+
+  // send email
+  new SendEmailPublisher(mqWrapper.channel).publish({
+    username: isUser.username,
+    receiverEmail: isUser.email,
+    template: "resetPasswordSuccess",
+  });
+
+  // send response
+  res.json({ message: "Password change succesfully" });
+};
+
+export default changePassword;
