08d18d2b0735a908501a74c74b318e0918d4688e
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-15 10:42:27 +0600

    fix: fix api middleware

--- 1-gateway-service/src/middlewares/api.middleware.ts
+++ 1-gateway-service/src/middlewares/api.middleware.ts
@@ -1,19 +1,31 @@
-import { AxiosService } from "@src/axios-services/axios";
-import { createMiddleware } from "hono/factory";
+import type { Request, Response, NextFunction } from "express";
+import { AxiosService } from "@src/axios-services/axios"; // Assuming this path is correct
 
+// Extend the Request interface to include your custom properties
+// This is necessary for TypeScript to recognize publicAxios and protectedAxios on req
+declare global {
+  namespace Express {
+    interface Request {
+      publicAxios?: AxiosService;
+      protectedAxios?: AxiosService;
+    }
+  }
+}
+
+/**
+ * Express middleware factory to attach AxiosService instances to the request object.
+ * @param baseURL The base URL for the Axios instances.
+ * @param serviceName The name of the service.
+ * @returns An Express middleware function.
+ */
 export const apiMiddleware = (baseURL: string, serviceName: string) => {
-  return createMiddleware<{
-    Variables: {
-      publicAxios: AxiosService;
-      protectedAxios: AxiosService;
-    };
-  }>(async (c, next) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     const publicInstance = await AxiosService.create(baseURL, serviceName);
-    const protedtedInstance = await AxiosService.create(baseURL, serviceName);
+    const protectedInstance = await AxiosService.create(baseURL, serviceName);
 
-    c.set("publicAxios", publicInstance);
-    c.set("protectedAxios", protedtedInstance);
+    req.publicAxios = publicInstance;
+    req.protectedAxios = protectedInstance;
 
-    await next();
-  });
+    next();
+  };
 };
