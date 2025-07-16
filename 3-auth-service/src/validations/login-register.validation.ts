53ed5ce15c19bf53b335eb8557cda1eaaec5b1f4
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-16 13:21:17 +0600

    feat: update login register validation

--- 3-auth-service/src/validations/login-register.validation.ts
+++ 3-auth-service/src/validations/login-register.validation.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 export const userRegistrationSchema = z.object({
   username: z.string(),
   email: z.string().email(),
-  password: z.string().min(8),
+  password: z.string().min(6, "Password must be atlest 6 char long"),
   profilePicture: z
     .string()
     .min(1, "Profile picture is required")
@@ -18,6 +18,6 @@ export type UserRegistrationInput = z.infer<typeof userRegistrationSchema>;
 export const userLoginSchema = z.object({
   username: z.string(),
   email: z.string().email(),
-  password: z.string().min(8),
+  password: z.string().min(6, "Password must be atlest 6 char long"),
 });
 export type UserLoginInput = z.infer<typeof userLoginSchema>;
