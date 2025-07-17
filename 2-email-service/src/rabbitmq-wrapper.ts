30133c34f89a945a5285e4eede38c6d6dc06240c
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-17 21:36:03 +0600

    feat: update rabbitmq wrapper

--- 2-email-service/src/rabbitmq-wrapper.ts
+++ 2-email-service/src/rabbitmq-wrapper.ts
@@ -1,29 +1,36 @@
-import { connect, type Channel, type ChannelModel } from 'amqplib';
+import { handleAsync } from "@fvoid/shared-lib";
+import { connect, type Channel, type ChannelModel } from "amqplib";
 
 class MQWrapper {
   private _connection?: ChannelModel;
   private _channel?: Channel;
 
   get channel() {
-    if (!this._channel) throw new Error('Cannot access channel before connecting');
+    if (!this._channel)
+      throw new Error("Cannot access channel before connecting");
 
     return this._channel;
   }
 
   get connection() {
-    if (!this._connection) throw new Error('Cannot access channel before connecting');
+    if (!this._connection)
+      throw new Error("Cannot access channel before connecting");
 
     return this._connection;
   }
 
   async connect(url: string) {
-    const connection = await connect(url);
+    this._connection = await handleAsync(
+      connect(url),
+      "RabbitMq Connection Error !!!"
+    );
 
-    this._connection = await connect(url);
+    this._channel = await handleAsync(
+      this._connection.createChannel(),
+      "RabbitMq Connection Error !!!"
+    );
 
-    this._channel = await connection.createChannel();
-
-    console.log('rabbitmq connected');
+    console.log("Rabbitmq Connected 🎉");
   }
 }
 
