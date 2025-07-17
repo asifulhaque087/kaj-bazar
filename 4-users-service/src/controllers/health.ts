22421ea5f62beab521a3fe38266bd76aa5ac464a
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-17 09:01:55 +0600

    chore: remove unwanted files

--- 4-users-service/src/controllers/health.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
-
-const health = (_req: Request, res: Response): void => {
-  res.status(StatusCodes.OK).send('Users service is healthy and OK.');
-};
-
-export { health };
