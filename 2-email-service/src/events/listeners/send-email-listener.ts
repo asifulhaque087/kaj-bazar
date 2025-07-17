6d6e31553d933d58d9ab0541eea91476bd6608f9
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-17 21:38:17 +0600

    fix: send email listener

--- 2-email-service/src/events/listeners/send-email-listener.ts
+++ 2-email-service/src/events/listeners/send-email-listener.ts
@@ -1,24 +1,31 @@
-import { Exchanges, Listener, Queues, RoutingKeys, SendEmailEvent } from '@fvoid/shared-lib';
-import { sendEmail } from '@notifications/queues/mail.transport';
-import type { ConsumeMessage } from 'amqplib';
+import {
+  Exchanges,
+  Listener,
+  Queues,
+  RoutingKeys,
+  type SendEmailEvent,
+} from "@fvoid/shared-lib";
+import { sendEmail } from "@src/queues/mail.transport";
+import type { ConsumeMessage } from "amqplib";
 
-import { config } from '@notifications/config';
+import { config } from "@src/config";
 
 export class SendEmailListener extends Listener<SendEmailEvent> {
   exchangeName: Exchanges.Mail_Exchange = Exchanges.Mail_Exchange;
   queueName: Queues.Mail_Queue = Queues.Mail_Queue;
   routingKey: RoutingKeys.SendEmail = RoutingKeys.SendEmail;
 
-  async onMessage(data: SendEmailEvent['data'], message: ConsumeMessage) {
-    const { receiverEmail, verifyLink, template, username, otp, resetLink } = data;
+  async onMessage(data: SendEmailEvent["data"], message: ConsumeMessage) {
+    const { receiverEmail, verifyLink, template, username, otp, resetLink } =
+      data;
 
     const locals = {
       appLink: `${config.CLIENT_URL}`,
-      appIcon: 'https://i.ibb.co/Kyp2m0t/cover.png',
+      appIcon: "https://i.ibb.co/Kyp2m0t/cover.png",
       username,
       verifyLink,
       resetLink,
-      otp
+      otp,
     };
 
     await sendEmail(template, receiverEmail, locals);
