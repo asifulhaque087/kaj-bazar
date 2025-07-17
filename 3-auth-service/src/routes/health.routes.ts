5578bbf99eb38bfd5aa75a7102487ebc4ff6ed3b
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-17 10:45:05 +0600

    refactor: rename to routes

--- 3-auth-service/src/routes/health.routes.ts
+++ 3-auth-service/src/routes/health.routes.ts
@@ -1,10 +1,10 @@
 // ** Third party imports
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
 
 const healthRouter = Router();
 
 // ** Check Health
-healthRouter.get("/auth-health", (_req, res) => {
+healthRouter.get("/auth-health", (_req: Request, res: Response) => {
   return res.send("Auth server is running");
 });
 
