db86f388cb568f71f5bf33eaca97989eddcfc947
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-17 10:47:06 +0600

    feat: update route with types

--- 4-users-service/src/routes/health.routes.ts
+++ 4-users-service/src/routes/health.routes.ts
@@ -1,11 +1,11 @@
 // ** Third party imports
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
 
 const healthRouter = Router();
 
 // ** Check Health
-healthRouter.get("/user-health", (_req, res) => {
-  return res.send("User server is running");
+healthRouter.get("/users-health", (_req: Request, res: Response) => {
+  return res.send("Users server is running");
 });
 
 export default healthRouter;
