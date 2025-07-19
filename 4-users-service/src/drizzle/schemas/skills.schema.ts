25c603cc24d5fa6d645898cc700e715bcfea207a
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-19 16:43:02 +0600

    feat: add skills schema

---
+++ 4-users-service/src/drizzle/schemas/skills.schema.ts
@@ -0,0 +1,20 @@
+import { SellersTable } from "@src/drizzle/schemas/sellers.schema";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, serial, varchar } from "drizzle-orm/pg-core";
+
+// ** --- Skills Table ---
+export const SkillsTable = pgTable("skills_table", {
+  id: serial("id").primaryKey(),
+  sellerId: integer("seller_id")
+    .notNull()
+    .references(() => SellersTable.id, { onDelete: "cascade" }),
+  skill: varchar("skill", { length: 255 }).notNull(),
+});
+
+// ** --- Relations for Drizzle ---
+export const skillsRelations = relations(SkillsTable, ({ one }) => ({
+  seller: one(SellersTable, {
+    fields: [SkillsTable.sellerId],
+    references: [SellersTable.id],
+  }),
+}));
