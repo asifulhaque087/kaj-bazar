d21e7ebecc7cc1bde45c9350eb5d1876e524f1a7
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-19 16:47:59 +0600

    fix: fix schemas path

--- 4-users-service/drizzle.config.ts
+++ 4-users-service/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "src/drizzle/migrations",
-  schema: "src/drizzle/schema.ts",
+  schema: "src/drizzle/schemas",
   dialect: "postgresql",
   dbCredentials: {
     url: config.DATABASE_URL,
