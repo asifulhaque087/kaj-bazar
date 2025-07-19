f1100fc8cd4a2f9353ba56c201f41d7a8ca643dc
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-19 16:45:29 +0600

    feat: add remove buyer listener

---
+++ 4-users-service/src/events/listeners/remove-buyers.listener.ts
@@ -0,0 +1,27 @@
+// ** Third Party Imports
+import type { ConsumeMessage } from "amqplib";
+import {
+  Exchanges,
+  handleAsync,
+  Listener,
+  Queues,
+  RoutingKeys,
+  type RemoveBuyersEvent,
+} from "@fvoid/shared-lib";
+
+// ** DB
+import { db } from "@src/drizzle/db";
+import { BuyersTable } from "@src/drizzle/schemas";
+
+export class RemoveBuyersListener extends Listener<RemoveBuyersEvent> {
+  exchangeName: Exchanges.Remove_Buyers_Exchange =
+    Exchanges.Remove_Buyers_Exchange;
+  queueName: Queues.Remove_Buyers_Queue = Queues.Remove_Buyers_Queue;
+  routingKey: RoutingKeys.RemoveBuyers = RoutingKeys.RemoveBuyers;
+
+  async onMessage(data: RemoveBuyersEvent["data"], message: ConsumeMessage) {
+    await handleAsync(db.delete(BuyersTable));
+
+    this.channel.ack(message);
+  }
+}
