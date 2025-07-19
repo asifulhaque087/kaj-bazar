d568a47c94217c09b79faaff1f0076d7712acb10
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-19 16:41:15 +0600

    feat: update seller routes

--- 4-users-service/src/routes/seller.routes.ts
+++ 4-users-service/src/routes/seller.routes.ts
@@ -8,13 +8,15 @@ import seedSeller from "@src/controllers/seed-seller.controller";
 import getSellerById from "@src/controllers/seller-by-id.controller";
 import getSellerByName from "@src/controllers/seller-by-name.controller";
 import updateSeller from "@src/controllers/update-seller.controller";
+import { validateData } from "@src/middlewares/validation.middleware";
+import { createSellerSchema } from "@src/validations/create-serller.validation";
 
 const sellerRouter = Router();
 
 sellerRouter.get("/id/:sellerId", getSellerById);
 sellerRouter.get("/username/:username", getSellerByName);
 sellerRouter.get("/random/:size", getRandomSellers);
-sellerRouter.post("/create", createSeller);
+sellerRouter.post("/create", validateData(createSellerSchema), createSeller);
 sellerRouter.put("/:sellerId", updateSeller);
 sellerRouter.put("/seed/:count", seedSeller);
 
