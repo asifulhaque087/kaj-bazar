d9dfee97dcec540f6ebc0d0544152df5ac2f1191
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-19 16:36:08 +0600

    feat: seed auth

--- 1-gateway-service/src/controllers/auth/auth-seed.controller.ts
+++ 1-gateway-service/src/controllers/auth/auth-seed.controller.ts
@@ -2,7 +2,23 @@ import type { Request, Response } from "express";
 import { type AxiosResponse, AxiosError } from "axios"; // Import AxiosResponse and AxiosError
 
 const authSeed = async (req: Request, res: Response) => {
-  return res.json({ m: "This is auth seeding " });
+  const { count } = req.params;
+
+  try {
+    const apiService = req.publicAxios!;
+    const response: AxiosResponse = await apiService.axios.put(
+      `/seed/${count}`
+    );
+    // Send response data
+    return res.json(response.data);
+  } catch (error) {
+    if (!(error instanceof AxiosError && error.response))
+      throw new Error("Server Error");
+
+    return res.status(error.response.status).json(error.response.data);
+  }
+
+  // return res.json({ m: "This is auth seeding " });
 };
 
 export default authSeed;
---
+++ 3-auth-service/src/controllers/seed-auth.controller.ts
@@ -0,0 +1,70 @@
+// ** Third party imports
+import type { Request, Response } from "express";
+import { faker } from "@faker-js/faker";
+import * as crypto from "node:crypto";
+import { handleAsync } from "@fvoid/shared-lib";
+
+// ** Utils
+import { hashPassword } from "@src/utils/hashing.util";
+
+// ** DB
+import { db } from "@src/drizzle/db";
+import { AuthTable } from "@src/drizzle/schema";
+import { RemoveBuyersPublisher } from "@src/events/publishers/remove-buyers.publisher";
+import { mqWrapper } from "@src/rabbitmq-wrapper";
+import { CreateBuyerPublisher } from "@src/events/publishers/create-buyer.publisher";
+
+const seedAuth = async (req: Request, res: Response) => {
+  const { count = "10" } = req.params;
+  const total = parseInt(count);
+
+  // Remove every users
+  await handleAsync(db.delete(AuthTable));
+
+  new RemoveBuyersPublisher(mqWrapper.channel).publish(null);
+
+  // Crate an event so users service removes all the buyer
+
+  for (let i = 0; i < total; i++) {
+    // Prepare Data
+
+    const authData = {
+      username: `test${i + 1}`,
+      email: `test${i + 1}@gmail.com`,
+      profilePublicId: crypto.randomUUID(),
+      password: await hashPassword("qwerty"),
+      country: faker.location.country(),
+      browserName: faker.internet.userAgent(),
+      deviceType: faker.helpers.arrayElement(["desktop", "mobile", "tablet"]),
+      profilePicture: faker.image.urlPicsumPhotos(),
+      emailVerificationToken: crypto.randomBytes(20).toString("hex"),
+      emailVerified: Math.random() < 0.5,
+    };
+
+    // Crate User
+
+    const result = await handleAsync(
+      db
+        .insert(AuthTable)
+        .values(authData)
+        .$returningId()
+        .then((res) => res[0])
+    );
+
+    // Send this to user service
+
+    new CreateBuyerPublisher(mqWrapper.channel).publish({
+      id: result?.id!,
+      username: authData.username,
+      email: authData.email,
+      profilePublicId: authData.profilePublicId,
+      profilePicture: authData.profilePicture,
+      country: authData.country,
+    });
+  }
+  return res.json({ message: "Seed users created successfully." });
+
+  // res.json({ message: "I am from seed auth data" });
+};
+
+export default seedAuth;
