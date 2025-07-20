6b13d83f3725881f9c42667b0771285c15e1d9b8
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-20 13:29:58 +0600

    feat: add get seller listener

---
+++ 4-users-service/src/events/listeners/get-sellers.listener.ts
@@ -0,0 +1,45 @@
+import {
+  Exchanges,
+  Listener,
+  Queues,
+  RoutingKeys,
+  type GetSellersEvent,
+} from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import { SellersTable } from "@src/drizzle/schemas";
+import { ReceiveSellersPublisher } from "@src/events/publishers/receive-sellers.publisher";
+import type { ConsumeMessage } from "amqplib";
+import { sql } from "drizzle-orm";
+
+export class GetSellerListener extends Listener<GetSellersEvent> {
+  exchangeName: Exchanges.Get_Sellers = Exchanges.Get_Sellers;
+  queueName: Queues.Get_Sellers = Queues.Get_Sellers;
+  routingKey: RoutingKeys.GetSellers = RoutingKeys.GetSellers;
+
+  async onMessage(data: GetSellersEvent["data"], message: ConsumeMessage) {
+    const { count } = data;
+
+    // ** --- Fetch random seller ---
+    const sellers = await db
+      .select({
+        id: SellersTable.id,
+        email: SellersTable.email,
+        username: SellersTable.username,
+        profilePicture: SellersTable.profilePicture,
+      })
+      .from(SellersTable)
+      .orderBy(sql`RANDOM()`);
+
+    // ** --- Publish an event ---
+    new ReceiveSellersPublisher(this.channel).publish({
+      count: count,
+      sellers: sellers,
+    });
+
+    this.channel.ack(message);
+  }
+}
+
+// 100
+
+// 30
