1956de88903f0d7d7b9b7ab99bf4bdb6e4ba0ec4
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-20 13:25:00 +0600

    feat: use seed gig controller

--- 1-gateway-service/src/routes/gig.routes.ts
+++ 1-gateway-service/src/routes/gig.routes.ts
@@ -1,14 +1,17 @@
 // import type { SessionData } from "@src/app";
 import { config } from "@src/config";
 import searchGig from "@src/controllers/gig/search.controller";
+import seedGigs from "@src/controllers/gig/seed-gigs.controller";
 import { apiMiddleware } from "@src/middlewares/api.middleware";
 import { Router } from "express";
 
 const gigRouter = Router();
 
-gigRouter.use(apiMiddleware(`${config.GIG_BASE_URL}/api/v1/gig`, "gig"));
+gigRouter.use(apiMiddleware(`${config.GIG_BASE_URL}/api/v1/gigs`, "gigs"));
 
-gigRouter.get("/gig/search/:from/:size/:type", searchGig);
+gigRouter.get("/search/:from/:size/:type", searchGig);
+
+gigRouter.put("/seed/:count", seedGigs);
 
 // searchRouter.get("/gig", async (c) => {
 //   console.log("hello from gig search");
