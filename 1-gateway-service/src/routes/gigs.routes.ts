1507f889df3aeb2104e9009d302e7530d7a44dee
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-21 14:54:43 +0600

    feat: add gig routes

---
+++ 1-gateway-service/src/routes/gigs.routes.ts
@@ -0,0 +1,24 @@
+// import type { SessionData } from "@src/app";
+import { config } from "@src/config";
+import searchGig from "@src/controllers/gigs/search.controller";
+import seedGigs from "@src/controllers/gigs/seed-gigs.controller";
+import { apiMiddleware } from "@src/middlewares/api.middleware";
+import { Router } from "express";
+
+const gigRouter = Router();
+
+gigRouter.use(apiMiddleware(`${config.GIG_BASE_URL}/api/v1/gigs`, "gigs"));
+
+gigRouter.get("/search/:from/:size/:type", searchGig);
+
+gigRouter.put("/seed/:count", seedGigs);
+
+// searchRouter.get("/gig", async (c) => {
+//   console.log("hello from gig search");
+
+//   return c.json({
+//     message: "sucess",
+//   });
+// });
+
+export default gigRouter;
