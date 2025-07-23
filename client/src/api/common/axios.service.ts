52f4ee320ddd4fc37ae1f531a0b56ccea55c6006
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-24 01:59:39 +0600

    feat: add api service

---
+++ client/src/api/common/axios.service.ts
@@ -0,0 +1,14 @@
+import { config } from "@/config";
+import axios, { type AxiosInstance } from "axios";
+
+export const apiService = (serviceRelativePath: string): AxiosInstance => {
+  const instance = axios.create({
+    baseURL: config.API_GATEWAY_URL + serviceRelativePath,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    withCredentials: true,
+  });
+  return instance;
+};
