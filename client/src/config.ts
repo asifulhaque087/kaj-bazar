cd9772b7ab5642c3310d4fdb2df710abdeb61706
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-24 01:58:37 +0600

    feat: update config

---
+++ client/src/config.ts
@@ -0,0 +1,5 @@
+export const config = {
+  // API_GATEWAY_URL: process.env.API_GATEWAY_URL || "http://gateway:4000",
+  API_GATEWAY_URL: "http://localhost:4000/api/v1/gateway",
+  CLIENT_URL: process.env.CLIENT_URL || "",
+};
