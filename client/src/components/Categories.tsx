3dc4e6b488ebca7b30d6e9e0ac7d5202d9e48aaa
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-24 02:02:42 +0600

    feat: update categories

--- client/src/components/Categories.tsx
+++ client/src/components/Categories.tsx
@@ -1,25 +1,36 @@
-const Categories = () => {
-  const categories = [
-    {
-      title: "Website Development",
-    },
-    { title: "Video Editing" },
+"use client";
 
-    { title: "Vibe Coding" },
-  ];
+import { useRouter } from "next/navigation";
+
+const categories = [
+  "Graphics & Design",
+  "Digital Marketing",
+  "Writing & Translation",
+  "Video & Animation",
+  "Music & Audio",
+  "Programming & Tech",
+  "Photography",
+  "Data",
+  "Business",
+];
+const Categories = () => {
+  const router = useRouter();
 
   return (
     <div className="flex items-center gap-x-[14px] overflow-auto  scrollbar-hide">
       {categories.map((cat, i) => (
         <button
+          onClick={() =>
+            router.push(`/gigs?category=${encodeURIComponent(cat)}`)
+          }
           key={i}
           className={`${
             i % 2 === 0
               ? "bg-[rgba(99,146,216,15%)]"
               : "bg-[rgba(39,201,190,15%)]"
-          } text-white px-[14px] py-[13px] text-[14px] tracking-[0.14px] font-[300] rounded-[8px] grid place-items-center capitalize whitespace-nowrap`}
+          } text-white px-[14px] py-[13px] text-[14px] tracking-[0.14px] font-[300] rounded-[8px] grid place-items-center capitalize whitespace-nowrap cursor-pointer`}
         >
-          {cat.title}
+          {cat}
         </button>
       ))}
     </div>
