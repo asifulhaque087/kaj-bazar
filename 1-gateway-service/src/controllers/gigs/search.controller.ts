c66da72a123681c2cd92834e63d5775018d3bb0b
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-24 01:56:10 +0600

    feat: update serch controller

--- 1-gateway-service/src/controllers/gigs/search.controller.ts
+++ 1-gateway-service/src/controllers/gigs/search.controller.ts
@@ -2,45 +2,33 @@ import type { Request, Response } from "express";
 import { type AxiosResponse, AxiosError } from "axios"; // Import AxiosResponse and AxiosError
 
 const searchGig = async (req: Request, res: Response) => {
-  const { from, size, type } = req.params;
-
-  const queryParams = req.query;
-
-  let queryString = "";
-
-  const objList = Object.entries(queryParams);
-  const lastItemIndex = objList.length - 1;
-
-  objList.forEach(([key, value], index) => {
-    queryString += `${key}=${String(value)}${
-      index !== lastItemIndex ? "&" : ""
-    }`;
-  });
-
-  //    return res.json(queryString);
-
   try {
-    // Todo: get the public api service
-
     const apiService = req.publicAxios!;
-
+    const queryString = objectToQueryString(req.query);
     const response: AxiosResponse = await apiService.axios.get(
-      `/gig/search/gig/${from}/${size}/${type}?${queryString})`
+      `/search/?${queryString}`
     );
 
-    return res.json(response);
-
-    //   return res.json({
-    //     message: response.data.message,
-    //     total: response.data.total,
-    //     gigs: response.data.gigs,
-    //   });
+    return res.json(response.data);
   } catch (error) {
     if (!(error instanceof AxiosError && error.response))
       throw new Error("Server Error");
-
     return res.status(error.response.status).json(error.response.data);
   }
 };
 
+const objectToQueryString = (obj: { [key: string]: any }): string => {
+  let query: string = "";
+  const objList: [string, any][] = Object.entries(obj);
+  const lastItemIndex: number = objList.length - 1;
+
+  objList.forEach(([key, value]: [string, any], index: number) => {
+    // It's good practice to encode URI components for URL safety
+    query += `${encodeURIComponent(key)}=${encodeURIComponent(value || "")}`;
+    if (index !== lastItemIndex) {
+      query += "&";
+    }
+  });
+  return query;
+};
 export default searchGig;
