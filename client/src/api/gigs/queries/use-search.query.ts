1b8f47b6eeff2034c30d0a2758662e96029d6317
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-24 16:13:54 +0600

    feat: add support for pagiantion

--- client/src/api/gigs/queries/use-search.query.ts
+++ client/src/api/gigs/queries/use-search.query.ts
@@ -1,11 +1,33 @@
+// import { searchGigs } from "@/api/gigs/queries/queries.apis";
+// import { keepPreviousData, useQuery } from "@tanstack/react-query";
+
+// export const useSearch = ({ q }: { q: string }) => {
+//   return useQuery({
+//     queryKey: ["gigs", q],
+//     queryFn: () => searchGigs(q),
+//     staleTime: 20000,
+//     placeholderData: keepPreviousData,
+//   });
+// };
+
 import { searchGigs } from "@/api/gigs/queries/queries.apis";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
-export const useSearch = ({ q }: { q: string }) => {
+export const useSearch = ({
+  q,
+  page,
+  limit,
+}: {
+  q: string;
+  page: number;
+  limit: number;
+}) => {
   return useQuery({
-    queryKey: ["gigs", q],
-    queryFn: () => searchGigs(q),
+    queryKey: ["gigs", q, page, limit], // Include page and limit in queryKey
+    queryFn: () => searchGigs(q, page, limit), // Pass page and limit to searchGigs
     staleTime: 20000,
     placeholderData: keepPreviousData,
+    // Add refetchOnWindowFocus: false if you don't want to refetch on window focus
+    // refetchOnWindowFocus: false,
   });
 };
