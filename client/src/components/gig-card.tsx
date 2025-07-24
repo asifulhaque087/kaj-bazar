95ae3e434e9d7625b2ef594c64e0e5fed816edaf
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-24 16:08:52 +0600

    feat: rename to gig card

--- client/src/components/gig-card.tsx
+++ client/src/components/gig-card.tsx
@@ -1,7 +1,20 @@
+import { Gig } from "@/api/gigs/schemas/gig.schema";
 import { Heart, Star } from "lucide-react";
 import Link from "next/link";
 
-export const Gig = ({ gig }: { gig: any }) => {
+interface IGigCard {
+  gig: Gig;
+}
+
+const GigCard = (props: IGigCard) => {
+  // ** --- Props ---
+  const { gig } = props;
+
+  console.log("gig is ", gig);
+
+  const avgRating = gig.ratingSum / gig.ratingsCount;
+  const rating = avgRating.toFixed(1);
+
   return (
     <>
       {/* main 200px 640 oile 150px  r tailwind a sm oise 767 porjonto mane 768 oile md suru */}
@@ -13,7 +26,8 @@ export const Gig = ({ gig }: { gig: any }) => {
           {/* left */}
           <div className="w-[24px] h-[24px] rounded-full overflow-hidden">
             <img
-              src="https://www.shutterstock.com/image-vector/vector-bright-portrait-beautiful-brunette-600nw-2452267975.jpg"
+              // src="https://www.shutterstock.com/image-vector/vector-bright-portrait-beautiful-brunette-600nw-2452267975.jpg"
+              src={gig.profilePicture!}
               alt="profile"
               className="object-center object-cover h-full w-full"
             />
@@ -22,7 +36,8 @@ export const Gig = ({ gig }: { gig: any }) => {
 
           <div>
             <h4 className="font-inter font-[600] text-[14px] text-[#222325]">
-              cc__creative
+              {/* cc__creative */}
+              {gig.username}
             </h4>
 
             <p className="font-inter font-[500] text-[12.48px] text-[#2E90EB] mt-[4px]">
@@ -37,8 +52,9 @@ export const Gig = ({ gig }: { gig: any }) => {
           <div className="px-[12px]">
             <div className="h-[148px] w-full rounded-[7px] overflow-hidden bg-red-500">
               <img
-                src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/435057859/original/b1178ebc894adffee3df9f46a3af8378c41f21c2/build-secure-and-scalable-backend-apis-with-nodejs-express.png"
-                alt="product"
+                // src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/435057859/original/b1178ebc894adffee3df9f46a3af8378c41f21c2/build-secure-and-scalable-backend-apis-with-nodejs-express.png"
+                src={gig.coverImage}
+                alt="gig"
                 className="h-full w-full  object-center object-cover"
               />
             </div>
@@ -62,10 +78,12 @@ export const Gig = ({ gig }: { gig: any }) => {
         </h1> */}
 
         <h1 className="font-inter font-[400] text-[16px] leading-[1.375] text-[#222325] px-[25px] h-[42px] overflow-hidden line-clamp-2">
-          I will design UI UX for mobile app with figma for ios I will design UI
+          {/* I will design UI UX for mobile app with figma for ios I will design UI
           UX for mobile app with figma for ios I will design UI UX for mobile
           app with figma for ios I will design UI UX for mobile app with figma
-          for ios
+          for ios */}
+
+          {gig.title}
         </h1>
 
         {/* Rating */}
@@ -77,13 +95,15 @@ export const Gig = ({ gig }: { gig: any }) => {
 
           {/* raing */}
           <span className="font-inter text-[12px] font-[700] text-[#FFBE5B] ">
-            5.0
+            {/* 5.0 */}
+            {rating}
           </span>
 
           {/* reviews */}
 
           <span className="font-inter font-[400] text-[12.48px] text-[#B5B6BA]">
-            32.4K
+            {/* 32.4K */}
+            {gig.ratingsCount}
           </span>
         </div>
 
@@ -94,7 +114,7 @@ export const Gig = ({ gig }: { gig: any }) => {
             starting at
           </p>
           <span className="text-[#2E90EB] font-inter font-[400] text-[12.26px]">
-            $8,674
+            {/* $8,674 */}${gig.price}
           </span>
         </div>
       </div>
@@ -102,4 +122,4 @@ export const Gig = ({ gig }: { gig: any }) => {
   );
 };
 
-export default Gig;
+export default GigCard;
