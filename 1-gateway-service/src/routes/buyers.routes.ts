b96c1cc8a95df40c308f8a3c792f0ff9c53d82de
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-26 20:38:03 +0600

    feat: add curren buye route

--- 1-gateway-service/src/routes/buyers.routes.ts
+++ 1-gateway-service/src/routes/buyers.routes.ts
@@ -1,5 +1,7 @@
 import { config } from "@src/config";
+import currentBuyer from "@src/controllers/buyers/curren-buyer.controller";
 import { apiMiddleware } from "@src/middlewares/api.middleware";
+import { verifyJwtToken } from "@src/middlewares/verify-jwt.middleware";
 import { Router } from "express";
 
 const buyerRouter = Router();
@@ -7,5 +9,7 @@ const buyerRouter = Router();
 buyerRouter.use(
   apiMiddleware(`${config.USERS_BASE_URL}/api/v1/buyers`, "users")
 );
+
+buyerRouter.get("/current-buyer", verifyJwtToken, currentBuyer);
 
 export default buyerRouter;
