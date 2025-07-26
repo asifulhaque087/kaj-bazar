3b4e8ac4a21dd405bdbb642b8e4c8a207dde77c2
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-26 20:38:46 +0600

    feat: add get gig by id in gateway

--- 1-gateway-service/src/routes/gigs.routes.ts
+++ 1-gateway-service/src/routes/gigs.routes.ts
@@ -1,5 +1,6 @@
 // import type { SessionData } from "@src/app";
 import { config } from "@src/config";
+import getGigById from "@src/controllers/gigs/gig-by-id.controller";
 import searchGig from "@src/controllers/gigs/search.controller";
 import seedGigs from "@src/controllers/gigs/seed-gigs.controller";
 import { apiMiddleware } from "@src/middlewares/api.middleware";
@@ -11,6 +12,7 @@ gigRouter.use(apiMiddleware(`${config.GIG_BASE_URL}/api/v1/gigs`, "gigs"));
 
 // gigRouter.get("/search/:from/:size/:type", searchGig);
 gigRouter.get("/search", searchGig);
+gigRouter.get("/:gigId", getGigById);
 
 gigRouter.put("/seed/:count", seedGigs);
 
