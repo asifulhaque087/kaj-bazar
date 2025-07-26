82c80c4a8450df9c8b755b5cb98c205a294be076
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-26 20:37:41 +0600

    feat: udpate seller controller

---
+++ 1-gateway-service/src/controllers/sellers/create-seller.controller.ts
@@ -0,0 +1,23 @@
+import type { Request, Response } from "express";
+import { type AxiosResponse, AxiosError } from "axios";
+
+const createSeller = async (req: Request, res: Response) => {
+  const body = req.body;
+
+  try {
+    const apiService = req.protectedAxios!;
+
+    const response: AxiosResponse = await apiService.axios.post(
+      `/create`,
+      body
+    );
+
+    return res.json(response.data);
+  } catch (error) {
+    if (!(error instanceof AxiosError && error.response))
+      throw new Error("Server Error");
+    return res.status(error.response.status).json(error.response.data);
+  }
+};
+
+export default createSeller;
