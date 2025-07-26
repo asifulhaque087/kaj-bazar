e2bec42e75ce7e9c82efbf4be5b40796f5ab3a32
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-26 20:40:07 +0600

    feat: deliver parameter

--- 3-auth-service/src/routes/identity.route.ts
+++ 3-auth-service/src/routes/identity.route.ts
@@ -5,15 +5,24 @@ import { Router } from "express";
 import currentUser from "@src/controllers/current-user.controller";
 import refreshToken from "@src/controllers/refresh-token.controller";
 import { verifyClientToken } from "@fvoid/shared-lib";
+import { config } from "@src/config";
 
 const identityRouter = Router();
 
 // Todo -  verifyClientToken(config.JWT_TOKEN),
 
 // ** Refresh token
-identityRouter.get("/refresh-token", verifyClientToken, refreshToken);
+identityRouter.get(
+  "/refresh-token",
+  verifyClientToken(config.JWT_TOKEN),
+  refreshToken
+);
 
 // ** Get Current User
-identityRouter.get("/currentuser", verifyClientToken, currentUser);
+identityRouter.get(
+  "/currentuser",
+  verifyClientToken(config.JWT_TOKEN),
+  currentUser
+);
 
 export default identityRouter;
