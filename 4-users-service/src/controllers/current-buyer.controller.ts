5357e4c86955092e1b4556868f0dfd5151e9d114
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-26 20:41:25 +0600

    feat: update current buyer controller

--- 4-users-service/src/controllers/current-buyer.controller.ts
+++ 4-users-service/src/controllers/current-buyer.controller.ts
@@ -1,7 +1,31 @@
+import {
+  handleAsync,
+  NotAuthorizedError,
+  NotFoundError,
+} from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import { BuyersTable } from "@src/drizzle/schemas";
+import { eq } from "drizzle-orm";
 import type { Request, Response } from "express";
 
-const currentBuyer = (req: Request, res: Response) => {
-  return res.json({ m: "I am from current buyer" });
+const currentBuyer = async (req: Request, res: Response) => {
+  // get current user email and find user
+  const buyer = req.user;
+
+  if (!buyer) throw new NotAuthorizedError();
+
+  const isBuyer = await handleAsync(
+    db.query.BuyersTable.findFirst({
+      where: eq(BuyersTable.email, buyer.email),
+    })
+  );
+
+  if (!isBuyer) throw new NotFoundError();
+
+  // return response
+  // return res.json({ message: "Authenticated user", user });
+  // return res.json({ user: isUser });
+  return res.json(isBuyer);
 };
 
 export default currentBuyer;
