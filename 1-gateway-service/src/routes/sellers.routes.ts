f53e597c2202b34bb14389c1ba998a9099d004c3
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-26 20:38:57 +0600

    feat: add seller route in gateway

--- 1-gateway-service/src/routes/sellers.routes.ts
+++ 1-gateway-service/src/routes/sellers.routes.ts
@@ -1,4 +1,5 @@
 import { config } from "@src/config";
+import createSeller from "@src/controllers/sellers/create-seller.controller";
 import seedSellers from "@src/controllers/sellers/seed-sellers.controller";
 import { apiMiddleware } from "@src/middlewares/api.middleware";
 import { Router } from "express";
@@ -9,6 +10,7 @@ sellerRouter.use(
   apiMiddleware(`${config.USERS_BASE_URL}/api/v1/sellers`, "users")
 );
 
+sellerRouter.post("/create", createSeller);
 sellerRouter.put("/seed/:count", seedSellers);
 
 export default sellerRouter;
