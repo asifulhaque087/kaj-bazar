5f101298df16bc8ff8861040121f9b934ef83f39
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-26 20:36:56 +0600

    refactor: remove comments

--- 1-gateway-service/src/controllers/auth/current-user.controller.ts
+++ 1-gateway-service/src/controllers/auth/current-user.controller.ts
@@ -2,9 +2,17 @@ import type { Request, Response } from "express";
 import { type AxiosResponse, AxiosError } from "axios"; // Import AxiosResponse and AxiosError
 
 const currentUser = async (req: Request, res: Response) => {
+  // console.log(
+  //   "hello I am from current user ####################################"
+  // );
+
   try {
     const apiService = req.protectedAxios!;
+
+    console.log("api service @#@#@#@#@#@#@#@#@#@@# ", apiService);
     const response: AxiosResponse = await apiService.axios.get("/currentuser");
+
+    console.log("api service after @#@#@#@#@#@#@#@#@#@@# ", apiService);
     // Send response data
     return res.json(response.data);
   } catch (error) {
