edbac2d05079650507d26b08f5f296e0e27e0ade
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-26 20:42:56 +0600

    feat: add curren buyer route

--- 4-users-service/src/routes/buyer.routes.ts
+++ 4-users-service/src/routes/buyer.routes.ts
@@ -5,11 +5,20 @@ import { Router } from "express";
 import getBuyerByEmail from "@src/controllers/buyer-by-email.controller";
 import getBuyerByName from "@src/controllers/buyer-by-name.controller";
 import currentBuyer from "@src/controllers/current-buyer.controller";
+import { verifyClientToken } from "@fvoid/shared-lib";
+import { config } from "@src/config";
 
 const buyerRouter = Router();
 
 buyerRouter.get("/email", getBuyerByEmail);
-buyerRouter.get("/username", currentBuyer);
+// buyerRouter.get("/username", currentBuyer);
+
+buyerRouter.get(
+  "/current-buyer",
+  verifyClientToken(config.JWT_TOKEN),
+  currentBuyer
+);
+
 buyerRouter.get("/:username", getBuyerByName);
 
 export default buyerRouter;
