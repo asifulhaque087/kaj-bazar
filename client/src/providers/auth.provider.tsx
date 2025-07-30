f3d833a64f8aa9211a3a8a68b798d5eb143028d3
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-30 18:07:22 +0600

    feat: update auth provider with new type

--- client/src/providers/auth.provider.tsx
+++ client/src/providers/auth.provider.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { useAuthUser } from "@/api/auth/queries/use-auth-user.query";
-import { useCurrentBuyer } from "@/api/buyers/queries/current-buyer/curren-buyer.query";
+import { useAuthUser } from "@/api/auth";
+import { useCurrentBuyer } from "@/api/buyers";
 import { useAuthStore } from "@/store/use-auth.store";
 import React, { useEffect } from "react";
 
@@ -10,7 +10,7 @@ const AuthProvider = ({
   children: React.ReactNode;
 }>) => {
   const { isLoading: isUserLoading, data: user } = useAuthUser();
-  const { isLoading: isBuyerLoading, data: buyer } = useCurrentBuyer();
+  const { isLoading: isBuyerLoading, data: buyer } = useCurrentBuyer(user?.id);
 
   const { setAuthUser, setBuyer } = useAuthStore();
 
@@ -19,7 +19,7 @@ const AuthProvider = ({
     if (buyer) setBuyer(buyer);
   }, [user, setAuthUser, buyer, setBuyer]);
 
-  if (isUserLoading || isBuyerLoading) return <div>Loading...</div>;
+  // if (isUserLoading || isBuyerLoading) return <div>Loading...</div>;
 
   return <div>{children}</div>;
 };
