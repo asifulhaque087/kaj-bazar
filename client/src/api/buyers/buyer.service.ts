c434cd2408d613f5a8e993891b5799de5fbdc8ad
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-30 17:53:43 +0600

    feat: add buyer service

---
+++ client/src/api/buyers/buyer.service.ts
@@ -0,0 +1,7 @@
+import { buyer__axios } from "@/api/common/axios.service";
+import { Buyer } from "@/schemas";
+
+export const currentBuyer = async (): Promise<Buyer> => {
+  const response = await buyer__axios.get<Buyer>(`/current-buyer`);
+  return response.data;
+};
