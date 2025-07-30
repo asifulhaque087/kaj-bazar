efe1825a6ec8ad1f6252407d9b2a5d92d4706c79
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-30 17:51:37 +0600

    feat: add auth service in client

---
+++ client/src/api/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { auth__axios } from "@/api/common/axios.service";
+import { Auth, LoginForm, RegisterPayload } from "@/schemas";
+
+// ** --- Queries ---
+export async function getAuthUser(): Promise<Auth> {
+  const response = await auth__axios.get<Auth>(`/currentuser`);
+  return response.data;
+}
+
+// ** --- Mutations ---
+export const login = async (data: LoginForm) => {
+  const response = await auth__axios.post(`/signin`, data);
+  return response.data;
+};
+
+export const register = async (data: RegisterPayload) => {
+  const response = await auth__axios.post(`/register`, data);
+  return response.data;
+};
