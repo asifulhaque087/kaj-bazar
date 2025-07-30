f5961e1b9b8fcede01e46c0922019f507712cf63
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-30 17:52:28 +0600

    chore: remove unwanted files

--- client/src/api/auth/schemas/login.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const loginValidation = z.object({
-  // username: z.string().min(1, "User name is required"),
-  email: z.string().email("Invalid email address."),
-  password: z.string().min(6, "Password must be at least 6 characters."),
-});
-
-export type LoginFormField = z.infer<typeof loginValidation>;
