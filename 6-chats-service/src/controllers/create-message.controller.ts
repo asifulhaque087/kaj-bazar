513ceed0b73ae258a9d5f4c3d3898bf9ba5c76f0
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-30 17:46:59 +0600

    feat: add create message controller in chat service

---
+++ 6-chats-service/src/controllers/create-message.controller.ts
@@ -0,0 +1,46 @@
+import { handleAsync } from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import { MessagesTable } from "@src/drizzle/schemas";
+import type { CreateMessageForm } from "@src/validations/message.validations";
+import type { Request, Response } from "express";
+
+const createMessage = async (req: Request, res: Response) => {
+  const {
+    buyerId,
+    conversationId,
+    receiverPicture,
+    receiverUsername,
+    sellerId,
+    senderPicture,
+    senderUsername,
+    body,
+    file,
+  } = req.body as CreateMessageForm;
+
+  // ** Prepare data
+
+  const messageData = {
+    buyerId,
+    conversationId,
+    receiverPicture,
+    receiverUsername,
+    sellerId,
+    senderPicture,
+    senderUsername,
+    body,
+  };
+
+  const result = await handleAsync(
+    db
+      .insert(MessagesTable)
+      .values(messageData)
+      .returning()
+      .then((res) => res[0])
+  );
+
+  // Todo - We have to publish this message a an event
+
+  return res.json({ m: "Event send successfully" });
+};
+
+export default createMessage;
