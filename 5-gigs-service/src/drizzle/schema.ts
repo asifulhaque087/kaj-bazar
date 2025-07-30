174ff6a48dab7c71c797dcf6796ec791e40c9c11
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-30 17:44:37 +0600

    feat: update gig schema primary to int to uuid

--- 5-gigs-service/src/drizzle/schema.ts
+++ 5-gigs-service/src/drizzle/schema.ts
@@ -1,28 +1,31 @@
 import {
   pgTable,
-  serial,
   text,
   boolean,
   integer,
   timestamp,
   jsonb,
+  uuid,
 } from "drizzle-orm/pg-core";
 
 export const GigsTable = pgTable("gigs_table", {
-  id: serial("id").primaryKey(), // Auto-incrementing primary key
-  sellerId: integer("seller_id").notNull(),
+  id: uuid("id").primaryKey().defaultRandom(),
+  sellerId: uuid("seller_id").notNull(),
   username: text("username").notNull(),
   email: text("email").notNull(),
-  profilePicture: text("profile_picture"),
+  profilePicture: text("profile_picture").notNull(),
   title: text("title").notNull(),
   description: text("description").notNull(),
   basicTitle: text("basic_title").notNull(),
   basicDescription: text("basic_description").notNull(),
   category: text("category").notNull(),
   subCategories: text("sub_categories").array().notNull(),
+  expectedDelivery: text("expected_delivery").notNull(),
+  coverImage: text("cover_image").notNull(),
+  price: integer("price").notNull(),
+  // ** Optional
   tags: text("tags").array(),
   active: boolean("active").notNull().default(true),
-  expectedDelivery: text("expected_delivery").notNull(),
   ratingsCount: integer("ratings_count").notNull().default(0),
   ratingSum: integer("rating_sum").notNull().default(0),
   ratingCategories: jsonb("rating_categories")
@@ -33,17 +36,15 @@ export const GigsTable = pgTable("gigs_table", {
       two: { star: number; count: number };
       one: { star: number; count: number };
     }>()
+    .notNull()
     .default({
       five: { star: 0, count: 0 },
       four: { star: 0, count: 0 },
       three: { star: 0, count: 0 },
       two: { star: 0, count: 0 },
       one: { star: 0, count: 0 },
-    })
-    .notNull(),
-  price: integer("price").notNull().default(0),
+    }),
   sortId: integer("sort_id"),
-  coverImage: text("cover_image").notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
 });
 
