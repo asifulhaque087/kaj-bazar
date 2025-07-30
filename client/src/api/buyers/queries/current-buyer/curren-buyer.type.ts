061f506347b9e12866277e315dc01252be444f28
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-30 17:56:56 +0600

    chore: remove unwanted files in client

--- client/src/api/buyers/queries/current-buyer/curren-buyer.type.ts
+++
@@ -1,8 +0,0 @@
-export interface Buyer {
-  username: string;
-  email: string;
-  profilePublicId: `${string}-${string}-${string}-${string}-${string}`;
-  country: string | null;
-  profilePicture: string;
-  isSeller: boolean;
-}
