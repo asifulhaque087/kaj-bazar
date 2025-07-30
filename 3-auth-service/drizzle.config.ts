f0e1cb6cdac56ea69d528b7d87d51fd8a0c29f53
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-30 17:38:20 +0600

    feat: update dialect to postgresql

--- 3-auth-service/drizzle.config.ts
+++ 3-auth-service/drizzle.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   out: "src/drizzle/migrations",
   schema: "src/drizzle/schema.ts",
-  dialect: "mysql",
+  dialect: "postgresql",
   dbCredentials: {
     url: config.DATABASE_URL,
   },
