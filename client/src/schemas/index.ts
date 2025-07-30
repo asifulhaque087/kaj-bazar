1b80fe983fd8de2a2daf95da45f5397b28bf316d
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-30 18:08:39 +0600

    feat: re-export schemas in client

---
+++ client/src/schemas/index.ts
@@ -0,0 +1,5 @@
+export * from "@/schemas/auth.schema";
+export * from "@/schemas/buyer.schema";
+export * from "@/schemas/seller.schema";
+export * from "@/schemas/gig.schema";
+export * from "@/schemas/chat.schema";
