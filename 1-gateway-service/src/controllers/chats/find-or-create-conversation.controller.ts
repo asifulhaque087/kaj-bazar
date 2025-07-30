0d7ec72e0677330260c377cfe5f4dcfbcc6026e7
asifulhaque087 <asifulhaque087@gmail.com>
2025-07-30 17:36:58 +0600

    feat: add find or create conversation in gateway

---
+++ 1-gateway-service/src/controllers/chats/find-or-create-conversation.controller.ts
@@ -0,0 +1,24 @@
+import type { Request, Response } from "express";
+import { type AxiosResponse, AxiosError } from "axios";
+
+const findOrCreateConversation = async (req: Request, res: Response) => {
+  const body = req.body;
+
+  console.log("hello 2 from controller");
+
+  try {
+    const apiService = req.protectedAxios!;
+    const response: AxiosResponse = await apiService.axios.post(
+      `/conversation/find-or-create`,
+      body
+    );
+
+    return res.json(response.data);
+  } catch (error) {
+    if (!(error instanceof AxiosError && error.response))
+      throw new Error("Server Error");
+    return res.status(error.response.status).json(error.response.data);
+  }
+};
+
+export default findOrCreateConversation;
