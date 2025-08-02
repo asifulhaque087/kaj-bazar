074c1f2e88486fb87b7c68639257d620966a2e44
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 14:23:59 +0600

    feat: add get conversation controller in gateway

---
+++ 1-gateway-service/src/controllers/chats/get-conversations.controller.ts
@@ -0,0 +1,21 @@
+import type { Request, Response } from "express";
+import { type AxiosResponse, AxiosError } from "axios";
+
+const getConversations = async (req: Request, res: Response) => {
+  const { username } = req.params;
+
+  try {
+    const apiService = req.protectedAxios!;
+    const response: AxiosResponse = await apiService.axios.get(
+      `/conversation/get-conversations/${username}`
+    );
+
+    return res.json(response.data);
+  } catch (error) {
+    if (!(error instanceof AxiosError && error.response))
+      throw new Error("Server Error");
+    return res.status(error.response.status).json(error.response.data);
+  }
+};
+
+export default getConversations;
