4e61471fcf197b42882d51e6009b4e754c7519b2
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 19:31:19 +0600

    feat: set other seller after fetching seller

--- client/src/api/sellers/seller.queries.ts
+++ client/src/api/sellers/seller.queries.ts
@@ -1,17 +1,21 @@
 import { getSellerByName } from "@/api/sellers/seller.service";
 import { UseSellerByNameProps } from "@/api/sellers/seller.types";
 import { useQueryWithSideEffects } from "@/hooks/useQueryWithSideEffects";
+import { useAuthStore } from "@/store/use-auth.store";
 
 export const useSellerByName = (props: UseSellerByNameProps) => {
+  // ** --- Props ---
   const { isBuyer, username, authUser } = props;
+
+  // ** --- Store ---
+  const { setOtherSeller } = useAuthStore();
 
   return useQueryWithSideEffects({
     queryKey: ["seller", username],
     queryFn: () => getSellerByName(username),
     enabled: !!authUser && isBuyer,
     onSuccess: (data) => {
-      console.log("@@@@@@@@@@@@ seller data is ", data);
-      // todo - setOtherSeller to the zustand store
+      setOtherSeller(data);
     },
   });
 };
