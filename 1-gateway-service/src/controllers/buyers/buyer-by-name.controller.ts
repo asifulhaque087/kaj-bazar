d9a5c0c76f6edfe8bbe3af4d65226f26072cd77c
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 14:23:30 +0600

    feat: add buyer by name controller in gateway

---
+++ 1-gateway-service/src/controllers/buyers/buyer-by-name.controller.ts
@@ -0,0 +1,23 @@
+import type { Request, Response } from "express";
+import { type AxiosResponse, AxiosError } from "axios";
+
+const getBuyerByName = async (req: Request, res: Response) => {
+  const { username } = req.params;
+
+  try {
+    const apiService = req.protectedAxios!;
+
+    const response: AxiosResponse = await apiService.axios.get(
+      `/username/${username}`
+    );
+
+    return res.json(response.data);
+  } catch (error) {
+    if (!(error instanceof AxiosError && error.response))
+      throw new Error("Server Error");
+
+    return res.status(error.response.status).json(error.response.data);
+  }
+};
+
+export default getBuyerByName;
