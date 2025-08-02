1636758072bdb11147b4218e98bec45e63611564
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 14:27:34 +0600

    feat: add getBuyerByName api

--- client/src/api/buyers/buyer.service.ts
+++ client/src/api/buyers/buyer.service.ts
@@ -5,3 +5,8 @@ export const currentBuyer = async (): Promise<Buyer> => {
   const response = await buyer__axios.get<Buyer>(`/current-buyer`);
   return response.data;
 };
+
+export const getBuyerByName = async (username: string) => {
+  const response = await buyer__axios.get<Buyer>(`/username/${username}`);
+  return response.data;
+};
