4c97322b7a708758f60019ce68275c61710d91f8
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 14:30:54 +0600

    feat: add getSellerByName api

--- client/src/api/sellers/seller.service.ts
+++ client/src/api/sellers/seller.service.ts
@@ -1,7 +1,12 @@
 import { seller__axios } from "@/api/common/axios.service";
-import { CreateSellerPayload } from "@/schemas/seller.schema";
+import { CreateSellerPayload, Seller } from "@/schemas";
 
 export const createSeller = async (data: CreateSellerPayload) => {
   const response = await seller__axios.post(`/create`, data);
+  return response.data;
+};
+
+export const getSellerByName = async (username: string) => {
+  const response = await seller__axios.get<Seller>(`/username/${username}`);
   return response.data;
 };
