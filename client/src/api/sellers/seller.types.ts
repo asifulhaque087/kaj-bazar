b1fc3ecce2e2055e68b33b6d98fdea96ff2a4281
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 14:31:04 +0600

    feat: add UseSellerByNameProps type

--- client/src/api/sellers/seller.types.ts
+++ client/src/api/sellers/seller.types.ts
@@ -1,7 +1,13 @@
-import { CreateSellerForm } from "@/schemas";
+import { Auth, CreateSellerForm } from "@/schemas";
 import { UseFormReset, UseFormSetError } from "react-hook-form";
 
 export interface UseCreateSellerProps {
   setError: UseFormSetError<CreateSellerForm>;
   reset: UseFormReset<CreateSellerForm>;
+}
+
+export interface UseSellerByNameProps {
+  username: string;
+  isBuyer: boolean;
+  authUser: Auth | null;
 }
