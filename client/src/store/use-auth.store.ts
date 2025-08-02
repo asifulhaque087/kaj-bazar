2a0760682a4cd1832b998290b365f8372e42934e
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 19:33:22 +0600

    feat: add other buyer & seller states & methods

--- client/src/store/use-auth.store.ts
+++ client/src/store/use-auth.store.ts
@@ -1,23 +1,31 @@
-import { Auth, Buyer } from "@/schemas";
+import { Auth, Buyer, Seller } from "@/schemas";
 import { create } from "zustand";
 
 type States = {
   //   authUser: Array<Auth> | [];
   authUser: Auth | null;
   buyer: Buyer | null;
+  otherBuyer: Buyer | null;
+  otherSeller: Seller | null;
   //   seller: null;
 };
 
 type Actions = {
   setAuthUser: (user: Auth) => void;
   setBuyer: (buyer: Buyer) => void;
+  setOtherBuyer: (buyer: Buyer) => void;
+  setOtherSeller: (seller: Seller) => void;
 };
 
 export const useAuthStore = create<States & Actions>((set) => ({
   authUser: null,
   buyer: null,
+  otherBuyer: null,
+  otherSeller: null,
   setAuthUser: (user: Auth) => set({ authUser: user }),
   setBuyer: (buyer: Buyer) => set({ buyer: buyer }),
+  setOtherBuyer: (buyer: Buyer) => set({ otherBuyer: buyer }),
+  setOtherSeller: (seller: Seller) => set({ otherSeller: seller }),
 
   // set(() => ({
   //   authUser: user,
