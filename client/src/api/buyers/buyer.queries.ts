8c2332618de3ffb50e7a9dda3074dc6ec4d1b332
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 19:30:52 +0600

    feat: process data in buyers  queries callback

--- client/src/api/buyers/buyer.queries.ts
+++ client/src/api/buyers/buyer.queries.ts
@@ -1,28 +1,36 @@
 import { currentBuyer, getBuyerByName } from "@/api/buyers/buyer.service";
 import { UseBuyerByNameProps } from "@/api/buyers/buyer.types";
 import { useQueryWithSideEffects } from "@/hooks/useQueryWithSideEffects";
-import { useQuery } from "@tanstack/react-query";
+import { useAuthStore } from "@/store/use-auth.store";
 
 export const useCurrentBuyer = (authId: string | undefined) => {
-  return useQuery({
+  // ** --- Store ---
+  const { setBuyer } = useAuthStore();
+
+  return useQueryWithSideEffects({
     queryKey: ["current_buyer"],
     queryFn: () => currentBuyer(),
-    // staleTime: 20000,
-    // staleTime: 5 * 60 * 1000,
     enabled: !!authId,
+
+    onSuccess: (data) => {
+      setBuyer(data);
+    },
   });
 };
 
 export const useBuyerByName = (props: UseBuyerByNameProps) => {
+  // ** --- Props ---
   const { isBuyer, username, authUser } = props;
+
+  // ** --- Store ---
+  const { setOtherBuyer } = useAuthStore();
 
   return useQueryWithSideEffects({
     queryKey: ["buyer", username],
     queryFn: () => getBuyerByName(username),
     enabled: !!authUser && !isBuyer,
     onSuccess: (data) => {
-      // console.log("@@@@@@@@@@ buyer data is ", data);
-      // todo - setOtherBuyer to the zustand store
+      setOtherBuyer(data);
     },
   });
 };
