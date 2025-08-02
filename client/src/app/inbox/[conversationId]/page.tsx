59a8d6adeb46c1bfd3f76b6c2729609a7dbcaf68
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 14:31:33 +0600

    feat: add conversationId page

---
+++ client/src/app/inbox/[conversationId]/page.tsx
@@ -0,0 +1,205 @@
+"use client";
+import { useGetConversationsById } from "@/api/chats";
+import { Button } from "@/components/ui/button";
+import { DevTool } from "@hookform/devtools";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { createMessageDefaultForm } from "@/forms";
+import { createMessageForm, CreateMessageForm } from "@/schemas";
+import { useAuthStore } from "@/store/use-auth.store";
+import { useChatStore } from "@/store/use-chat.store";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ImageIcon, XCircle } from "lucide-react";
+import Image from "next/image";
+import { useParams, useSearchParams } from "next/navigation";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useBrowser } from "@/hooks/use-browser.hook";
+import { useSellerByName } from "@/api/sellers";
+import { useBuyerByName } from "@/api/buyers";
+
+const page = () => {
+  // ** --- Props ---
+
+  // ** --- States ---
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+
+  // ** --- Reference ---
+  const dummy = useRef<HTMLSpanElement>(null);
+
+  // ** --- Store ---
+  const { messages, selectedConversation } = useChatStore();
+  const { authUser, buyer } = useAuthStore();
+
+  // ** --- Params
+
+  const searchParams = useSearchParams();
+  const otherUser = searchParams.get("other") || "";
+  const { conversationId } = useParams<{ conversationId: string }>();
+
+  // ** --- Queries
+  const { data: conversation } = useGetConversationsById({ conversationId });
+
+  const currentUserIsBuyer =
+    conversation?.senderUsername === authUser?.username;
+
+  const {} = useBuyerByName({
+    isBuyer: currentUserIsBuyer,
+    username: otherUser,
+    authUser,
+  });
+  const {} = useSellerByName({
+    isBuyer: currentUserIsBuyer,
+    username: otherUser,
+    authUser,
+  });
+
+  // ** --- Use form ---
+  const form = useForm<CreateMessageForm>({
+    resolver: zodResolver(createMessageForm),
+    // defaultValues: createMessageDefaultForm(null, null, null),
+    defaultValues: { body: "" },
+    mode: "onSubmit",
+  });
+
+  useEffect(() => {
+    if (buyer && selectedConversation) {
+      const hola = createMessageDefaultForm(
+        selectedConversation?.id!,
+        buyer,
+        null
+      );
+
+      console.log("hola is ", hola);
+      form.reset(hola);
+    }
+  }, [buyer, form, selectedConversation]);
+
+  const isbrowser = useBrowser();
+
+  if (!isbrowser) return;
+
+  return (
+    <div>
+      <main>
+        {messages.map((msg) => (
+          <div key={msg.id}>{msg.body}</div>
+        ))}
+        <span ref={dummy}></span>
+      </main>
+
+      {/* form */}
+
+      <Form {...form}>
+        <form
+          // onSubmit={form.handleSubmit(onSubmit)}
+
+          onSubmit={form.handleSubmit((data) => console.log(data))}
+          // onSubmit={form.handleSubmit((data) => console.log("data is ", data))}
+          className="space-y-8 max-w-3xl w-full mx-auto py-10"
+        >
+          {/* body */}
+
+          <FormField
+            control={form.control}
+            name="body"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Send a message" type="text" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          {/* 
+          <FormField
+            control={form.control}
+            name="file"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="profile-picture-input">
+                  <ImageIcon />
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="hidden"
+                    id="profile-picture-input" // Added an ID for direct access
+                    type="file"
+                    accept="image/*" // Accept only image files
+                    onChange={handleFileChange}
+                  />
+                </FormControl>
+                {imagePreview && (
+                  <div className="mt-4 relative w-[100px] h-[100px]">
+                    <p className="text-sm text-gray-500 mb-2">Image Preview:</p>
+                    <Image
+                      src={imagePreview}
+                      alt="Profile Preview"
+                      layout="fill" // Use fill for better image handling within a container
+                      objectFit="cover"
+                      className="rounded-md"
+                    />
+                    <Button
+                      type="submit"
+                      onClick={handleClearImage}
+                      className="absolute -top-2 -right-2 bg-red-500 hover:bg-red-600 rounded-full p-1 h-auto w-auto"
+                      size="icon"
+                    >
+                      <XCircle className="h-4 w-4 text-white" />
+                    </Button>
+                  </div>
+                )}
+              </FormItem>
+            )}
+          /> */}
+
+          <Button type="submit">Submit</Button>
+        </form>
+
+        <DevTool control={form.control} />
+      </Form>
+    </div>
+  );
+
+  function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const base64String = reader.result as string;
+        setImagePreview(base64String);
+        form.setValue("file", base64String); // Set the base64 string to the form field
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setImagePreview(null);
+      form.setValue("file", "");
+    }
+  }
+
+  function handleClearImage() {
+    setImagePreview(null);
+    form.setValue("file", "");
+
+    // ** IMPORTANT: Clear the file input's value directly
+    const fileInput = document.getElementById(
+      "profile-picture-input"
+    ) as HTMLInputElement;
+    if (fileInput) {
+      fileInput.value = ""; // This clears the visually displayed file name
+    }
+  }
+
+  function onSubmit(values: CreateMessageForm) {
+    console.log("the values are ", values);
+  }
+};
+
+export default page;
