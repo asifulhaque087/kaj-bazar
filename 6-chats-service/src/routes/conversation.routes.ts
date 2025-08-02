f7db2ddf3435d6e19502507baa81160838ab7e80
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 14:26:15 +0600

    feat: update conversation route in chat service

--- 6-chats-service/src/routes/conversation.routes.ts
+++ 6-chats-service/src/routes/conversation.routes.ts
@@ -2,6 +2,8 @@ import findOrCreateConversation from "@src/controllers/find-or-create-conversati
 import { findOrCreateConversationSchema } from "@src/validations/conversation.validations";
 import { Router } from "express";
 import { validateData } from "@fvoid/shared-lib";
+import getConversations from "@src/controllers/get-conversations.controller";
+import getConversationById from "@src/controllers/conversation-by-id.controller";
 
 const conversationRouter = Router();
 
@@ -9,6 +11,16 @@ conversationRouter.post(
   "/conversation/find-or-create",
   validateData(findOrCreateConversationSchema),
   findOrCreateConversation
+);
+
+conversationRouter.get(
+  "/conversation/get-conversations/:username",
+  getConversations
+);
+
+conversationRouter.get(
+  "/conversation/get-conversation/:id",
+  getConversationById
 );
 
 export default conversationRouter;
