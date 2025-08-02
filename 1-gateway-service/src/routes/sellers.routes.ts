af70bbe3ca55c4fa7742af01c58d7e66e315badd
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 14:24:50 +0600

    feat: update seller route in gateway

--- 1-gateway-service/src/routes/sellers.routes.ts
+++ 1-gateway-service/src/routes/sellers.routes.ts
@@ -1,7 +1,9 @@
 import { config } from "@src/config";
 import createSeller from "@src/controllers/sellers/create-seller.controller";
 import seedSellers from "@src/controllers/sellers/seed-sellers.controller";
+import getSellerByName from "@src/controllers/sellers/seller-by-name.controller";
 import { apiMiddleware } from "@src/middlewares/api.middleware";
+import { verifyJwtToken } from "@src/middlewares/verify-jwt.middleware";
 import { Router } from "express";
 
 const sellerRouter = Router();
@@ -12,5 +14,6 @@ sellerRouter.use(
 
 sellerRouter.post("/create", createSeller);
 sellerRouter.put("/seed/:count", seedSellers);
+sellerRouter.get("/username/:username", verifyJwtToken, getSellerByName);
 
 export default sellerRouter;
