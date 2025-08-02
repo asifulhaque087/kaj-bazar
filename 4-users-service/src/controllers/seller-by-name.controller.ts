c14d34c535f6e2fee230f7926d0dff939d26ee23
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 14:25:13 +0600

    feat: add seller by name controller in users service

--- 4-users-service/src/controllers/seller-by-name.controller.ts
+++ 4-users-service/src/controllers/seller-by-name.controller.ts
@@ -1,7 +1,17 @@
+import { handleAsync } from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import { SellersTable } from "@src/drizzle/schemas";
+import { eq } from "drizzle-orm";
 import type { Request, Response } from "express";
 
-const getSellerByName = (req: Request, res: Response) => {
-  return res.json({ m: "I am from get seller by name" });
+const getSellerByName = async (req: Request, res: Response) => {
+  const { username } = req.params;
+
+  const [seller] = await handleAsync(
+    db.select().from(SellersTable).where(eq(SellersTable.username, username!))
+  );
+
+  return res.json(seller);
 };
 
 export default getSellerByName;
