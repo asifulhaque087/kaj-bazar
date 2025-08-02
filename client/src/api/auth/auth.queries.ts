4b2aeda84e8a388985118f46b354b458878779fc
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 19:29:08 +0600

    feat: set auth user after auth user query

--- client/src/api/auth/auth.queries.ts
+++ client/src/api/auth/auth.queries.ts
@@ -1,12 +1,24 @@
 import { getAuthUser } from "@/api/auth/auth.service";
-import { useQuery } from "@tanstack/react-query";
+import { useQueryWithSideEffects } from "@/hooks/useQueryWithSideEffects";
+import { useAuthStore } from "@/store/use-auth.store";
+import { useChatStore } from "@/store/use-chat.store";
+// import { useQuery } from "@tanstack/react-query";
 
 export const useAuthUser = () => {
-  return useQuery({
+  // ** --- store ---
+  const { connectSocket } = useChatStore();
+  const { setAuthUser } = useAuthStore();
+
+  return useQueryWithSideEffects({
     queryKey: ["auth_user"],
     queryFn: () => getAuthUser(),
     staleTime: Infinity,
     gcTime: Infinity,
     retry: false,
+
+    onSuccess: (data) => {
+      setAuthUser(data);
+      connectSocket();
+    },
   });
 };
