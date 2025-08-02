83ea94bac19455432b33c40de86b47bebb9f1900
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-02 19:32:48 +0600

    feat: simplify auth provider

--- client/src/providers/auth.provider.tsx
+++ client/src/providers/auth.provider.tsx
@@ -1,23 +1,20 @@
 "use client";
 import { useAuthUser } from "@/api/auth";
 import { useCurrentBuyer } from "@/api/buyers";
-import { useAuthStore } from "@/store/use-auth.store";
-import React, { useEffect } from "react";
+import { ReactNode } from "react";
 
 const AuthProvider = ({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) => {
   const { isLoading: isUserLoading, data: user } = useAuthUser();
   const { isLoading: isBuyerLoading, data: buyer } = useCurrentBuyer(user?.id);
 
-  const { setAuthUser, setBuyer } = useAuthStore();
-
-  useEffect(() => {
-    if (user) setAuthUser(user);
-    if (buyer) setBuyer(buyer);
-  }, [user, setAuthUser, buyer, setBuyer]);
+  // useEffect(() => {
+  //   if (user) setAuthUser(user);
+  //   if (buyer) setBuyer(buyer);
+  // }, [user, setAuthUser, buyer, setBuyer]);
 
   // if (isUserLoading || isBuyerLoading) return <div>Loading...</div>;
 
