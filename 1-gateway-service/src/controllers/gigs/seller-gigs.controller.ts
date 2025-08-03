e688f7124b2c21cd582b4da9679dce9de1d3e321
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-03 19:08:25 +0600

    feat: add seller gigs controlerl in gateway service

---
+++ 1-gateway-service/src/controllers/gigs/seller-gigs.controller.ts
@@ -0,0 +1,23 @@
+import type { Request, Response } from "express";
+import { type AxiosResponse, AxiosError } from "axios";
+
+const getSellerGigs = async (req: Request, res: Response) => {
+  const { sellerId } = req.params;
+
+  try {
+    const apiService = req.protectedAxios!;
+
+    const response: AxiosResponse = await apiService.axios.get(
+      `/seller/${sellerId}`
+    );
+
+    return res.json(response.data);
+  } catch (error) {
+    if (!(error instanceof AxiosError && error.response))
+      throw new Error("Server Error");
+
+    return res.status(error.response.status).json(error.response.data);
+  }
+};
+
+export default getSellerGigs;
