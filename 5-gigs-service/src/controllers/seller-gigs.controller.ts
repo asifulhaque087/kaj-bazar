c8196c2cc7f083ec5a74e843377203866067e100
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-03 19:09:32 +0600

    feat: add seller gigs functionality

--- 5-gigs-service/src/controllers/seller-gigs.controller.ts
+++ 5-gigs-service/src/controllers/seller-gigs.controller.ts
@@ -1,7 +1,19 @@
+import { handleAsync } from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import { GigsTable } from "@src/drizzle/schema";
+import { eq } from "drizzle-orm";
 import type { Request, Response } from "express";
 
-const getSellerGigs = (req: Request, res: Response) => {
-  return res.json({ m: "I am from Get Seller Gigs" });
+const getSellerGigs = async (req: Request, res: Response) => {
+  const { sellerId } = req.params;
+
+  const gigs = await handleAsync(
+    db.select().from(GigsTable).where(eq(GigsTable.sellerId, sellerId!))
+  );
+
+  return res.json(gigs);
+
+  // return res.json({ m: "I am from Get Seller Gigs" });
 };
 
 export default getSellerGigs;
