2d7081e005cd9f92c1160ccfb1a3d7fb92e44324
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-03 19:11:54 +0600

    feat: add useGetGigBySellerId query

--- client/src/api/gigs/gig.queries.ts
+++ client/src/api/gigs/gig.queries.ts
@@ -1,5 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import { getGigById, searchGigs } from "@/api/gigs/gig.service";
+import {
+  getGigById,
+  getGigBySellerId,
+  searchGigs,
+} from "@/api/gigs/gig.service";
+import { useQueryWithSideEffects } from "@/hooks/useQueryWithSideEffects";
 
 export const useGetGigById = ({ id }: { id: string }) => {
   return useQuery({
@@ -20,5 +25,13 @@ export const useSearch = ({
   return useQuery({
     queryKey: ["gigs", q, page, limit], // Include page and limit in queryKey
     queryFn: () => searchGigs(q, page, limit), // Pass page and limit to searchGigs
+  });
+};
+
+export const useGetGigBySellerId = (sellerId: string | undefined) => {
+  return useQueryWithSideEffects({
+    queryKey: ["gig", sellerId],
+    queryFn: () => getGigBySellerId(sellerId!),
+    enabled: !!sellerId,
   });
 };
