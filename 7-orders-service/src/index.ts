8f5a0655b15566e7a88d707a712fd5c4bf503750
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-05 21:21:01 +0600

    fix: fix & update the server of order service

--- 7-orders-service/src/index.ts
+++ 7-orders-service/src/index.ts
@@ -1,5 +1,5 @@
 // ** Third Party Imports
-import express, { type Application } from "express";
+import express, { type Application, type Request } from "express";
 import "express-async-errors";
 import cors from "cors";
 import morgan from "morgan";
@@ -11,16 +11,26 @@ import {
 } from "@fvoid/shared-lib";
 import http from "http";
 
-import { Server } from "socket.io";
-
 // ** Local Imports
 import { config } from "@src/config";
 import { mqWrapper } from "@src/rabbitmq-wrapper";
 import healthRouter from "@src/routes/health.routes";
 import queryRouter from "@src/routes/query.routes";
 import mutationRouter from "@src/routes/mutation.routes";
-// ** Define Service
+import { Server } from "socket.io";
+// import { createClient } from "redis";
+// import { createAdapter } from "@socket.io/redis-adapter";
 
+// Extend Req with IO
+declare global {
+  namespace Express {
+    interface Request {
+      io?: Server; // Add the 'io' property
+    }
+  }
+}
+
+// ** Define Service
 class Service {
   private app: Application;
 
@@ -28,14 +38,15 @@ class Service {
     this.app = app;
   }
 
-  public start() {
+  public async start() {
     this.load_configurations();
     this.set_standard_middlewares();
     this.set_security_middlewares();
-    this.set_route_middlewares();
-    // this.start_rabbitmq();
-    this.set_error_middlewares();
     this.start_server();
+    this.set_route_middlewares();
+    // await this.start_rabbitmq();
+    this.set_error_middlewares();
+    // this.start_server();
   }
 
   private load_configurations() {
@@ -60,12 +71,12 @@ class Service {
       })
     );
 
-    this.app.use(verifyGatewayToken(config.GATEWAY_JWT_TOKEN, "users"));
+    this.app.use(verifyGatewayToken(config.GATEWAY_JWT_TOKEN, "orders"));
   }
 
   private set_route_middlewares() {
     this.app.use(morgan("dev"));
-    const BASE_PATH = "/api/v1/chats";
+    const BASE_PATH = "/api/v1/orders";
     this.app.use(healthRouter);
     this.app.use(BASE_PATH, queryRouter);
     this.app.use(BASE_PATH, mutationRouter);
@@ -87,11 +98,31 @@ class Service {
     this.app.use(errorHandler);
   }
 
-  private start_server() {
+  // private start_server() {
+  //   const PORT = 4006;
+
+  //   this.app.listen(PORT, () => {
+  //     console.log(`Orders server is running on port ${PORT}`);
+  //   });
+
+  //   initializeGatewayClient();
+  // }
+
+  private async start_server() {
+    // create express app
+    // create http server
+    // start http server
+    // create io using http server
+    // create pub sub using io
+    // create a new instance of socket using io
+
     const PORT = 4006;
 
     const httpServer = new http.Server(this.app);
-    // const socketIO = await createSocketIO(httpServer);
+
+    httpServer.listen(PORT, () => {
+      console.log(`Orders server running on port ${PORT}`);
+    });
 
     const io = new Server(httpServer, {
       cors: {
@@ -99,16 +130,19 @@ class Service {
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       },
     });
+    // const pubClient = createClient({ url: config.REDIS_HOST });
+    // const subClient = pubClient.duplicate();
+    // await Promise.all([pubClient.connect(), subClient.connect()]);
+    // io.adapter(createAdapter(pubClient, subClient));
 
-    httpServer.listen(PORT, () => {
-      console.log(`Chats server running on port ${PORT}`);
+    // const socketIoApp = new SocketIOAppHandler(io);
+    // socketIoApp.listen();
+
+    // Attach io to the req
+    this.app.use("*", function (req, res, next) {
+      req.io = io; // Attach the initialized io instance
+      next();
     });
-
-    // startHttpServer(httpServer);
-
-    // this.app.listen(PORT, () => {
-    //   console.log(`Chats server is running on port ${PORT}`);
-    // });
   }
 }
 
@@ -118,5 +152,4 @@ const app = express();
 
 const server = new Service(app);
 
-server.start();
-
+await server.start();
