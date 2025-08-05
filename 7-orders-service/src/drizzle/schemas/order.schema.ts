5e16f55b170adcce96337d5a80ec2c70ea94d794
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-06 01:17:36 +0600

    feat: add order schema

---
+++ 7-orders-service/src/drizzle/schemas/order.schema.ts
@@ -0,0 +1,100 @@
+import {
+  boolean,
+  integer,
+  jsonb,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+interface ExtensionRequest {
+  oldDate: string;
+  newDate: string;
+  reason: string | null;
+  tempReason: string | null;
+  accepted: boolean | null;
+}
+
+interface DeliveredWork {
+  message?: string;
+  file: string;
+}
+
+interface Review {
+  rating: number;
+  review: string;
+  receivedAt: string;
+}
+
+interface User {
+  id: string;
+  username: string;
+  email: string;
+  profilePicture: string;
+  country?: string | null | undefined;
+}
+
+interface Gig {
+  id: string;
+  title: string;
+  basicTitle: string;
+  description: string;
+  basicDescription: string;
+  coverImage: string;
+}
+
+// interface Seller {
+//   rating: number;
+//   review: string;
+//   receivedAt: string;
+// }
+
+const OrderStatus = pgEnum("oder_status", [
+  "incomplete",
+  "progress",
+  "complete",
+]);
+
+// --- Order Table ---
+export const OrdersTable = pgTable("orders_table", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  messageId: uuid("message_id").notNull(),
+  placeOrderAt: timestamp("place_order_at", { withTimezone: true }).notNull(),
+
+  // requirement
+  requirement: text("requirement"),
+  requirementAt: timestamp("requirement_at"),
+
+  orderStartedAt: timestamp("order_started_at"),
+
+  gig: jsonb("gig").$type<Gig>(),
+  // buyer: jsonb("buyer").$type<Buyer>(),
+  // seller: jsonb("seller").$type<Seller>(),
+  buyer: jsonb("buyer").$type<User>(),
+  seller: jsonb("seller").$type<User>(),
+
+  // due dates
+  requestExtensions: jsonb("request_extensions").$type<ExtensionRequest[]>(),
+  deliveryDueDate: timestamp("delivery_due_date"),
+
+  // delivery
+  deliveredWorks: jsonb("delivered_works").$type<DeliveredWork[]>(),
+  orderDeliveredAt: timestamp("oder_delivered_at"),
+
+  // review
+  sellerReceivedReview: jsonb("seller_received_review").$type<Review>(),
+  buyerReceivedReview: jsonb("buyer_received_review").$type<Review>(),
+
+  accepted: boolean("accepted"),
+  acceptedAt: timestamp("accepted_at"),
+
+  orderStatus: OrderStatus("order_status").default("incomplete").notNull(),
+
+  paymentIntent: text("payment_intent"),
+  price: integer("price").notNull(),
+  serviceFee: integer("service_fee").notNull(),
+});
+
+// --- Relationships ---
