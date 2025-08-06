fe7cad334fd3d8e2c6bd34b50954a26187452c21
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-06 15:46:58 +0600

    feat: add message by id controller in gateway

---
+++ 1-gateway-service/src/controllers/chats/message-by-id.controller.ts
@@ -0,0 +1,21 @@
+import type { Request, Response } from "express";
+import { type AxiosResponse, AxiosError } from "axios";
+
+const getMessageById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const apiService = req.protectedAxios!;
+    const response: AxiosResponse = await apiService.axios.get(
+      `/message/${id}`
+    );
+
+    return res.json(response.data);
+  } catch (error) {
+    if (!(error instanceof AxiosError && error.response))
+      throw new Error("Server Error");
+    return res.status(error.response.status).json(error.response.data);
+  }
+};
+
+export default getMessageById;
