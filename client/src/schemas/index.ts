b79a767e02d88d4ba944093e3d579dd5933e49cb
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-06 15:54:27 +0600

    feat: re-export all order schemas

--- client/src/schemas/index.ts
+++ client/src/schemas/index.ts
@@ -3,3 +3,4 @@ export * from "@/schemas/buyer.schema";
 export * from "@/schemas/seller.schema";
 export * from "@/schemas/gig.schema";
 export * from "@/schemas/chat.schema";
+export * from "@/schemas/order.schema";
