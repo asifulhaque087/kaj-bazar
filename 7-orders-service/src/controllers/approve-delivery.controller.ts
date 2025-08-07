05e03cc235009b573b70cd5890fb6cdbfe4a640a
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-07 17:52:32 +0600

    feat: add approve delivery controller in order service

---
+++ 7-orders-service/src/controllers/approve-delivery.controller.ts
@@ -0,0 +1,25 @@
+import type { Request, Response } from "express";
+import { handleAsync } from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import { OrdersTable } from "@src/drizzle/schemas";
+import { eq } from "drizzle-orm";
+import type { ApproveDeliveryInput } from "@src/validations/order.validation";
+
+const approveDelivery = async (req: Request, res: Response) => {
+  const { id } = req.body as ApproveDeliveryInput;
+
+  const [order] = await handleAsync(
+    db
+      .update(OrdersTable)
+      .set({
+        accepted: true,
+        acceptedAt: new Date(),
+      })
+      .where(eq(OrdersTable.id, id!))
+      .returning()
+  );
+
+  return res.json(order);
+};
+
+export default approveDelivery;
