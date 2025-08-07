5670e6fe77e5f15adc96b72f9ff04983c030b3c7
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-07 17:33:04 +0600

    feat: add deliver work controller in order service

---
+++ 7-orders-service/src/controllers/deliver-work.controller.ts
@@ -0,0 +1,51 @@
+import type { Request, Response } from "express";
+import { handleAsync, uploads } from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import type { DeliverWorkInput } from "@src/validations/order.validation";
+import { OrdersTable } from "@src/drizzle/schemas";
+import { eq } from "drizzle-orm";
+
+const deliverWork = async (req: Request, res: Response) => {
+  const { id, file, message } = req.body as DeliverWorkInput;
+
+  // Retrieve the existing order to get the current deliveredWorks array
+  const [existingOrder] = await handleAsync(
+    db
+      .select({ deliveredWorks: OrdersTable.deliveredWorks })
+      .from(OrdersTable)
+      .where(eq(OrdersTable.id, id))
+  );
+
+  if (!existingOrder) {
+    return res.status(404).json({ error: "Order not found" });
+  }
+
+  const uploadResult = await uploads(file);
+
+  // Create the new delivered work object
+  const newDeliveredWork = {
+    file: uploadResult?.secure_url,
+    ...(message && { message }),
+  };
+
+  // Construct the updated deliveredWorks array
+  const updatedDeliveredWorks = existingOrder.deliveredWorks
+    ? [...existingOrder.deliveredWorks, newDeliveredWork]
+    : [newDeliveredWork];
+
+  // Update the order in the database with the new deliveredWorks and timestamp
+  const [order] = await handleAsync(
+    db
+      .update(OrdersTable)
+      .set({
+        deliveredWorks: updatedDeliveredWorks,
+        orderDeliveredAt: new Date(),
+      })
+      .where(eq(OrdersTable.id, id))
+      .returning()
+  );
+
+  return res.json(order);
+};
+
+export default deliverWork;
