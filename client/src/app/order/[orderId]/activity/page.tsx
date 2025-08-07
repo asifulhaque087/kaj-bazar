607144275976f048ffb521c93c3b2ee2985cf97f
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-07 17:34:51 +0600

    feat: rendering modal & files

--- client/src/app/order/[orderId]/activity/page.tsx
+++ client/src/app/order/[orderId]/activity/page.tsx
@@ -1,8 +1,136 @@
+"use client";
+
+import { useGetOrderById } from "@/api/orders";
+import DeliveryModal from "@/components/deliver-modal";
+import { Button } from "@/components/ui/button";
+import { DeliveredWork, Order } from "@/schemas";
+import { useAuthStore } from "@/store/use-auth.store";
+import { useParams } from "next/navigation";
+import { useState } from "react";
 
 const page = () => {
+  const { orderId } = useParams<{ orderId: string }>();
+
+  // ** --- States ---
+
+  const [deliverModal, setDeliverModal] = useState(false);
+
+  // ** --- store ---
+  const { authUser } = useAuthStore();
+
+  const { data: order, isLoading: isOrderLoading } = useGetOrderById(orderId);
+
+  if (isOrderLoading || !authUser) return;
+
+  // console.log("order from activity is ", order);
+  const isBuyer = authUser?.id === order?.buyer.id;
+
+  // useE
+
+  console.log("isbuyer is ", authUser, order, isBuyer);
+
   return (
-    <div>this is order activity page</div>
-  )
+    <div>
+      {!isBuyer && (
+        <div>
+          <Button onClick={() => setDeliverModal(true)}>deliver order</Button>
+
+          <DeliveryModal
+            orderId={orderId}
+            showModal={deliverModal}
+            setShowModal={setDeliverModal}
+          />
+        </div>
+      )}
+      {/* place order */}
+      <div>
+        {" "}
+        {isBuyer ? "You" : order?.buyer.username} Placed the order at:{" "}
+        {order?.placeOrderAt}
+      </div>
+      {/* {isBuyer ? (
+          <div>You Placed the order at: {order?.placeOrderAt}</div>
+        ) : (
+          <div>
+            {order?.buyer.username} Placed the order at: {order?.placeOrderAt}
+          </div>
+        )} */}
+      {/* requirements */}
+      {!!order?.requirement && (
+        <div>
+          <div>
+            {isBuyer ? "You" : order?.buyer.username} submitted the requireemtns
+            at: {order?.placeOrderAt}
+          </div>
+          <div>{order?.requirement}</div>
+        </div>
+      )}
+      {/* order started */}
+      <div>
+        {" "}
+        {isBuyer ? "Your" : "The"} order started at: {order?.placeOrderAt}
+      </div>
+      {/* due date */}
+      The delivery due date is {order?.deliveryDueDate}
+      {/* extension request*/}
+      {/* delivered works */}
+      {!!order?.deliveredWorks && <Works order={order} isBuyer={isBuyer} />}
+      {/* {!!order?.deliveredWorks } */}
+    </div>
+  );
+};
+
+export default page;
+
+interface WorksProps {
+  order: Order;
+  isBuyer: boolean;
 }
 
-export default page
+const Works = (props: WorksProps) => {
+  const { order, isBuyer } = props;
+
+  return (
+    <div>
+      {isBuyer ? (
+        <div>
+          {order.seller.username} Delivered your order at: {order?.placeOrderAt}
+        </div>
+      ) : (
+        <div>You delivered the order at: {order?.placeOrderAt}</div>
+      )}
+
+      <div>
+        {order.deliveredWorks?.map((work, i) => (
+          <div key={i}>file {i}</div>
+        ))}
+      </div>
+
+      {!order.accepted && isBuyer && (
+        <div>
+          <h1 className="text-[18px] font-bold">
+            Are you ready to approve the delivery ?
+          </h1>
+          <p>If you have issue contact the seller in the inbox</p>
+          <Button className="bg-green-400 text-white">
+            Yes, approve the delivery
+          </Button>
+        </div>
+      )}
+
+      {!order.accepted && !isBuyer && (
+        <h1 className="text-[18px] font-bold">
+          Waiting for order to be approved
+        </h1>
+      )}
+
+      {order.accepted && isBuyer && (
+        <h1 className="text-[18px] font-bold">Your order was completed</h1>
+      )}
+
+      {order.accepted && !isBuyer && (
+        <h1 className="text-[18px] font-bold">The order was completed</h1>
+      )}
+    </div>
+  );
+};
