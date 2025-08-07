b08c9bb9964c1d47509157195f59569b49e93c49
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-07 13:59:10 +0600

    feat: add getOrderById & startOrder api

--- client/src/api/orders/order.service.ts
+++ client/src/api/orders/order.service.ts
@@ -1,7 +1,17 @@
 import { order__axios } from "@/api/common/axios.service";
-import { CreateOrderPayload, Order } from "@/schemas";
+import { CreateOrderPayload, Order, StartOrderPayload } from "@/schemas";
 
 export const createOrder = async (data: CreateOrderPayload) => {
   const response = await order__axios.post<Order>(`/create`, data);
+  return response.data;
+};
+
+export const getOrderById = async (id: string) => {
+  const response = await order__axios.get<Order>(`/${id}`);
+  return response.data;
+};
+
+export const startOrder = async (data: StartOrderPayload) => {
+  const response = await order__axios.post<Order>(`/start-order`, data);
   return response.data;
 };
