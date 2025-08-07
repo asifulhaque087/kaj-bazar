ea7e3d6b638bc321afaa29d47f43621329995b61
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-07 13:56:29 +0600

    feat: add start order controller in order service

---
+++ 7-orders-service/src/controllers/start-order.controller.ts
@@ -0,0 +1,30 @@
+// ** Third Party Imports
+import type { Request, Response } from "express";
+
+// ** Local Imports
+import { handleAsync } from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import type { StartOrderInput } from "@src/validations/order.validation";
+import { OrdersTable } from "@src/drizzle/schemas";
+import { eq } from "drizzle-orm";
+
+const startOrder = async (req: Request, res: Response) => {
+  const { id, requirement } = req.body as StartOrderInput;
+
+  const [order] = await handleAsync(
+    db
+      .update(OrdersTable)
+      .set({
+        orderStartedAt: new Date(),
+        orderStatus: "progress",
+        requirement: requirement ? requirement : null,
+        requirementAt: requirement ? new Date() : null,
+      })
+      .where(eq(OrdersTable.id, id))
+      .returning()
+  );
+
+  return res.json(order);
+};
+
+export default startOrder;
