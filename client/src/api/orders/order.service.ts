bb555f71715a013209180aaecc0ce31bbf9a6522
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-07 17:34:08 +0600

    feat: add deliverWork api

--- client/src/api/orders/order.service.ts
+++ client/src/api/orders/order.service.ts
@@ -1,5 +1,10 @@
 import { order__axios } from "@/api/common/axios.service";
-import { CreateOrderPayload, Order, StartOrderPayload } from "@/schemas";
+import {
+  CreateOrderPayload,
+  DeliveredWorkPayload,
+  Order,
+  StartOrderPayload,
+} from "@/schemas";
 
 export const createOrder = async (data: CreateOrderPayload) => {
   const response = await order__axios.post<Order>(`/create`, data);
@@ -13,5 +18,10 @@ export const getOrderById = async (id: string) => {
 
 export const startOrder = async (data: StartOrderPayload) => {
   const response = await order__axios.post<Order>(`/start-order`, data);
+  return response.data;
+};
+
+export const deliverWork = async (data: DeliveredWorkPayload) => {
+  const response = await order__axios.post<Order>(`/deliver-work`, data);
   return response.data;
 };
