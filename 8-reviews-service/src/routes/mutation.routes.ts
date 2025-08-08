dab8d5bd4e2e45a47db50bb8f14cb7e53f9c01f3
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-08 16:39:43 +0600

    feat: add mutations for review in review service

--- 8-reviews-service/src/routes/mutation.routes.ts
+++ 8-reviews-service/src/routes/mutation.routes.ts
@@ -1,13 +1,32 @@
-import { Router, type Request, type Response } from "express";
+import { validateData } from "@fvoid/shared-lib";
+import buyerReviewSeller from "@src/controllers/buyer-review-seller.controller";
+import sellerReviewBuyer from "@src/controllers/seller-review-buyer.controller";
+import {
+  buyerReviewSellerSchema,
+  sellerReviewBuyerSchema,
+} from "@src/validations/review.validation";
+import { Router } from "express";
 
 const mutationRouter = Router();
 
 mutationRouter.post(
-  "/",
-
-  (req: Request, res: Response) => {
-    return res.json({ m: "I am from create review" });
-  }
+  "/buyer-review-seller",
+  validateData(buyerReviewSellerSchema),
+  buyerReviewSeller
 );
+
+mutationRouter.post(
+  "/seller-review-buyer",
+  validateData(sellerReviewBuyerSchema),
+  sellerReviewBuyer
+);
+
+// mutationRouter.post(
+//   "/",
+
+//   (req: Request, res: Response) => {
+//     return res.json({ m: "I am from create review" });
+//   }
+// );
 
 export default mutationRouter;
