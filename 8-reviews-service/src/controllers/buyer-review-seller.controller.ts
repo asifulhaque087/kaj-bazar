cb21f249ecc61e49a3d001da77c7b1290eac82fb
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-08 16:37:42 +0600

    feat: update buyerReviewSeller controller in review service

--- 8-reviews-service/src/controllers/buyer-review-seller.controller.ts
+++ 8-reviews-service/src/controllers/buyer-review-seller.controller.ts
@@ -2,20 +2,59 @@
 import type { Request, Response } from "express";
 
 // ** Local Imports
-import { handleAsync } from "@fvoid/shared-lib";
+import { BadRequestError, handleAsync } from "@fvoid/shared-lib";
 import { db } from "@src/drizzle/db";
 import type { BuyerReviewSellerInput } from "@src/validations/review.validation";
 import { ReviewsTable } from "@src/drizzle/schemas";
+import { SellerReceivedReviewPublisher } from "@src/events/publishers/seller-received-review.publisher";
+import { mqWrapper } from "@src/rabbitmq-wrapper";
 
 const buyerReviewSeller = async (req: Request, res: Response) => {
   const reviewData = req.body as BuyerReviewSellerInput;
+  // const {
+  //   buyerId,
+  //   gigId,
+  //   gigImage,
+  //   gigTitle,
+  //   orderId,
+  //   ratings,
+  //   receiverId,
+  //   sellerId,
+  //   senderId,
+  //   senderImage,
+  //   senderUsername,
+  //   comment,
+  //   senderCountry,
+  // } = req.body as BuyerReviewSellerInput;
 
   // Prepare auth data
-  // const reviewData = {};
 
   const [review] = await handleAsync(
     db.insert(ReviewsTable).values(reviewData).returning()
   );
+
+  if (!review) throw new BadRequestError("review error");
+
+  // ** Publish Event
+  new SellerReceivedReviewPublisher(mqWrapper.channel).publish(review);
+
+  //
+  // new SellerReceivedReviewPublisher(mqWrapper.channel).publish({
+  //   buyerId: review?.buyerId,
+  //   comment: review?.comment,
+  //   gigId: review?.gigId,
+  //   gigImage: review?.gigImage,
+  //   gigTitle: review?.gigTitle,
+  //   orderId: review?.orderId,
+  //   ratings: review?.ratings,
+  //   receiverId: review?.receiverId,
+  //   reivewGivenAt: review?.reivewGivenAt,
+  //   sellerId: review?.sellerId,
+  //   senderCountry: review?.senderCountry,
+  //   senderId: review?.senderId,
+  //   senderImage: review?.senderImage,
+  //   senderUsername: review?.senderUsername,
+  // });
 
   return res.json(review);
 };
