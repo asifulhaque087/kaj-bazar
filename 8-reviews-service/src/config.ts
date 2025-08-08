21945ae6c72f5b30e68d3a88e97de845226b4879
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-08 12:24:29 +0600

    feat: update config for db

--- 8-reviews-service/src/config.ts
+++ 8-reviews-service/src/config.ts
@@ -1,9 +1,10 @@
 // config.js
 export const config = {
-  DATABASE_HOST: process.env.DATABASE_HOST || "",
-  DATABASE_USER: process.env.DATABASE_USER || "",
-  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || "",
-  DATABASE_NAME: process.env.DATABASE_NAME || "",
+  // DATABASE_HOST: process.env.DATABASE_HOST || "",
+  // DATABASE_USER: process.env.DATABASE_USER || "",
+  // DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || "",
+  // DATABASE_NAME: process.env.DATABASE_NAME || "",
+  DATABASE_URL: process.env.DATABASE_URL || "",
   NODE_ENV: process.env.NODE_ENV || "",
   RABBITMQ_ENDPOINT: process.env.RABBITMQ_ENDPOINT || "",
   JWT_TOKEN: process.env.JWT_TOKEN || "",
@@ -12,6 +13,5 @@ export const config = {
   CLOUD_API_SECRET: process.env.CLOUD_API_SECRET || "",
   GATEWAY_JWT_TOKEN: process.env.GATEWAY_JWT_TOKEN || "",
   API_GATEWAY_URL: process.env.API_GATEWAY_URL || "",
-  ELASTIC_SEARCH_URL: process.env.ELASTIC_SEARCH_URL || "",
   CLUSTER_TYPE: process.env.CLUSTER_TYPE || "",
 };
