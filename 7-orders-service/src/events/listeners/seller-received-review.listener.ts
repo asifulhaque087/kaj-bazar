ea2096b7a7d48397c6383422aedace159dffd8de
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-09 16:02:35 +0600

    feat: add functionality to seller received review listener in order service

--- 7-orders-service/src/events/listeners/seller-received-review.listener.ts
+++ 7-orders-service/src/events/listeners/seller-received-review.listener.ts
@@ -1,21 +1,43 @@
 import {
   Exchanges,
   FanoutListener,
+  handleAsync,
   type Seller_Received_Review_Event,
+  type SellerReceivedReviewQueues,
 } from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import { OrdersTable } from "@src/drizzle/schemas";
 import type { ConsumeMessage } from "amqplib";
+import { eq } from "drizzle-orm";
 
 export class SellerReceivedReviewListener extends FanoutListener<Seller_Received_Review_Event> {
   exchangeName: Exchanges.Seller_Received_Review =
     Exchanges.Seller_Received_Review;
-  queueName: "seller-received-review-queue-order" =
-    "seller-received-review-queue-order";
+
+  queueName: SellerReceivedReviewQueues = "seller-received-review-queue-order";
 
   async onMessage(
     data: Seller_Received_Review_Event["data"],
     message: ConsumeMessage
   ) {
-    console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% order service ", data);
+    const { orderId, ratings, comment, reivewGivenAt } = data;
+
+    const reviewObject = {
+      rating: ratings,
+      review: comment,
+      // receivedAt: reivewGivenAt.toISOString(),
+      receivedAt: new Date(reivewGivenAt).toISOString(),
+      // receivedAt: reivewGivenAt,
+    };
+
+    await handleAsync(
+      db
+        .update(OrdersTable)
+        .set({
+          sellerReceivedReview: reviewObject,
+        })
+        .where(eq(OrdersTable.id, orderId))
+    );
 
     this.channel.ack(message);
   }
