b9826690da74001b6e9e2dac88c9d9d869641db7
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-09 16:03:34 +0600

    feat: add type for queue in user service

--- 4-users-service/src/events/listeners/buyer-received-review.listener.ts
+++ 4-users-service/src/events/listeners/buyer-received-review.listener.ts
@@ -2,14 +2,14 @@ import {
   Exchanges,
   FanoutListener,
   type Buyer_Received_Review_Event,
+  type BuyerReceivedReviewQueues,
 } from "@fvoid/shared-lib";
 import type { ConsumeMessage } from "amqplib";
 
 export class BuyerReceivedReviewListener extends FanoutListener<Buyer_Received_Review_Event> {
   exchangeName: Exchanges.Buyer_Received_Review =
     Exchanges.Buyer_Received_Review;
-  queueName: "buyer-received-review-queue-user" =
-    "buyer-received-review-queue-user";
+  queueName: BuyerReceivedReviewQueues = "buyer-received-review-queue-user";
 
   async onMessage(
     data: Buyer_Received_Review_Event["data"],
