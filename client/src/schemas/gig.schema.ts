8706b314a8546005e3d17b6b812c691b679d14ad
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-20 16:14:57 +0600

    feat: update validation error messages

--- client/src/schemas/gig.schema.ts
+++ client/src/schemas/gig.schema.ts
@@ -1,23 +1,44 @@
+// gig schema
+
 import { z } from "zod";
+
+const SubCategorySchema = z.object({
+  title: z.string().min(1, "Sub-category title cannot be empty"),
+});
+
+// Define the schema for a single Tag object
+const TagSchema = z.object({
+  title: z.string().min(1, "Tag title cannot be empty"),
+});
 
 export const gigSchema = z.object({
   id: z.string().uuid(),
-  sellerId: z.string().uuid().min(1, "Seller id is required"),
-  username: z.string().min(1, "User name is required"),
-  email: z.string().email().min(1, "Email is required"),
-  profilePicture: z.string().url().min(1, "profile image is required"),
-  title: z.string().min(1, "Gig title is required"),
-  description: z.string().min(1, "Description is required"),
-  basicTitle: z.string().min(1, "Basic title is required"),
-  basicDescription: z.string().min(1, "Basic description is required"),
-  category: z.string().min(1, "category is required"),
-  subCategories: z.array(z.string()),
-  expectedDelivery: z.string().min(1, "expected delivery is required"),
-  coverImage: z.string().url().min(1, "gig image is required"),
-  price: z.number().int().gt(1, "Gig price is required"),
+  sellerId: z.string().uuid("Invalid seller ID format"),
+  username: z.string().min(3, "Username must be at least 3 characters long"),
+  email: z.string().email("Invalid email address"),
+  profilePicture: z.string().url("Invalid URL for profile picture"),
+  title: z.string().min(5, "Title must be at least 5 characters long"),
+  description: z
+    .string()
+    .min(20, "Description must be at least 20 characters long"),
+  basicTitle: z
+    .string()
+    .min(5, "Basic title must be at least 5 characters long"),
+  basicDescription: z
+    .string()
+    .min(20, "Basic description must be at least 20 characters long"),
+  category: z.string().min(1, "Category cannot be empty"),
+  subCategories: z
+    .array(SubCategorySchema)
+    .min(1, "At least one sub-category is required"),
+  expectedDelivery: z.string().min(1, "Expected delivery cannot be empty"),
+  coverImage: z.string().min(1, "Cover image is required"),
+  // price: z.number().int().positive("Price must be a positive integer"),
+  // price: z.number(),
+  price: z.coerce.number().int().positive("Price must be a positive integer"),
 
   // ** optional
-  tags: z.array(z.string()).nullable().optional(),
+  tags: z.array(TagSchema).nullable().optional(),
   active: z.boolean().optional(),
   ratingsCount: z.number().int().optional(), // Drizzle has default(0), so optional on creation
   ratingSum: z.number().int().optional(), // Drizzle has default(0), so optional on creation
