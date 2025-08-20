686741b97ffa024c8a3e36a671dc120bd2a04631
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-20 16:10:37 +0600

    feat: add functionality of create gig controller in gig service

--- 5-gigs-service/src/controllers/create-gig.controller.ts
+++ 5-gigs-service/src/controllers/create-gig.controller.ts
@@ -1,7 +1,26 @@
+import { uploads } from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import { GigsTable } from "@src/drizzle/schema";
+import type { InsertGigInput } from "@src/validations/create-gig.validation";
 import type { Request, Response } from "express";
 
-const createGig = (req: Request, res: Response) => {
-  return res.json({ m: "I am from create Gig" });
+const createGig = async (req: Request, res: Response) => {
+  // extract the data
+  const formData = req.body as InsertGigInput;
+
+  // upload image to cloudinary
+  const uploadResult = await uploads(formData.coverImage);
+
+  // prepare data
+  const gigData = {
+    ...formData,
+    coverImage: uploadResult?.secure_url,
+  };
+
+  // create a new gig
+  const [newGig] = await db.insert(GigsTable).values(gigData).returning();
+
+  return res.json(newGig);
 };
 
 export default createGig;
