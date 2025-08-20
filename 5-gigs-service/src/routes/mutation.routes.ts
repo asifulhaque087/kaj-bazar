bc6e98354139f8ca526304484a55efcda5499be6
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-20 16:11:43 +0600

    feat: add create gig route mutation in gig service

--- 5-gigs-service/src/routes/mutation.routes.ts
+++ 5-gigs-service/src/routes/mutation.routes.ts
@@ -1,24 +1,23 @@
+import createGig from "@src/controllers/create-gig.controller";
 import seedGigs from "@src/controllers/seed-gigs.controller";
 import { Router, type Request, type Response } from "express";
 
 const mutationRouter = Router();
 
-mutationRouter.post("/create", (req: Request, res: Response) => {
-  return res.json({ m: "I am from Create Gigs" });
+mutationRouter.post("/create", createGig);
+
+mutationRouter.put("/seed/:count", seedGigs);
+
+mutationRouter.put("/active/:gigId", (req: Request, res: Response) => {
+  return res.json({ m: "I am from get  active gig by gig  id" });
 });
 
 mutationRouter.put("/:gigId", (req: Request, res: Response) => {
   return res.json({ m: "I am from Update Gig" });
 });
 
-mutationRouter.put("/active/:gigId", (req: Request, res: Response) => {
-  return res.json({ m: "I am from get  active gig by gig  id" });
-});
-
 mutationRouter.delete("/:gigId/:sellerId", (req: Request, res: Response) => {
   return res.json({ m: "I am from Delete Gig" });
 });
-
-mutationRouter.put("/seed/:count", seedGigs);
 
 export default mutationRouter;
