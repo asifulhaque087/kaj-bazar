53d38a8502c737b97a78205cb5a66479532912f6
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-22 02:01:34 +0600

    feat: add update gig controller in gateway service

---
+++ 1-gateway-service/src/controllers/gigs/update-gig.controller.ts
@@ -0,0 +1,19 @@
+import type { Request, Response } from "express";
+import { type AxiosResponse, AxiosError } from "axios";
+
+const updateGig = async (req: Request, res: Response) => {
+  const body = req.body;
+
+  try {
+    const apiService = req.protectedAxios!;
+    const response: AxiosResponse = await apiService.axios.put(`/update`, body);
+
+    return res.json(response.data);
+  } catch (error) {
+    if (!(error instanceof AxiosError && error.response))
+      throw new Error("Server Error");
+    return res.status(error.response.status).json(error.response.data);
+  }
+};
+
+export default updateGig;
