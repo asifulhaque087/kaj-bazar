a3937e7f23c99ec9c1c694c28d544e9fd0c74fa8
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-22 02:03:11 +0600

    feat: add useUpdateGig hook

--- client/src/api/gigs/gig.mutations.ts
+++ client/src/api/gigs/gig.mutations.ts
@@ -1,6 +1,6 @@
-import { createGig } from "@/api/gigs/gig.service";
-import { UseCreateGigProps } from "@/api/gigs/gig.types";
-import { CreateGigPayload } from "@/schemas";
+import { createGig, updateGig } from "@/api/gigs/gig.service";
+import { UseCreateGigProps, UseUpdateGigProps } from "@/api/gigs/gig.types";
+import { CreateGigPayload, UpdateGigPayload } from "@/schemas";
 import { ApiValidationError } from "@/types";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
@@ -16,6 +16,27 @@ export const useCreateGig = (props: UseCreateGigProps) => {
     onSuccess: (data) => {
       toast.success("Gig created successfully");
       reset();
+    },
+
+    onError: (error: AxiosError) => {
+      const { errors } = error.response?.data as ApiValidationError;
+      errors.forEach((err) =>
+        setError(err.field as "root", { message: err.message })
+      );
+    },
+  });
+};
+
+export const useUpdateGig = (props: UseUpdateGigProps) => {
+  // ** Props
+  const { setError } = props;
+
+  return useMutation({
+    mutationFn: (data: UpdateGigPayload) => updateGig(data),
+
+    onSuccess: (data) => {
+      toast.success("Gig updated successfully");
+      // reset();
     },
 
     onError: (error: AxiosError) => {
