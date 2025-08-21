2d6a574ffb62e13de021c3742c8fbd1198497cbf
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-22 02:03:48 +0600

    feat: add UseUpdateGigProps type

--- client/src/api/gigs/gig.types.ts
+++ client/src/api/gigs/gig.types.ts
@@ -1,7 +1,13 @@
-import { CreateGigForm } from "@/schemas";
+import { CreateGigForm, UpdateGigForm } from "@/schemas";
 import { UseFormReset, UseFormSetError } from "react-hook-form";
 
 export interface UseCreateGigProps {
   setError: UseFormSetError<CreateGigForm>;
   reset: UseFormReset<CreateGigForm>;
+}
+
+
+export interface UseUpdateGigProps {
+  setError: UseFormSetError<UpdateGigForm>;
+  // reset: UseFormReset<CreateGigForm>;
 }
