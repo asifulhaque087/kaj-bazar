a7109c0bb30437f7a308bb573cd9a194f9439f10
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-05 15:07:56 +0600

    feat: persist role in zustand store

--- client/src/store/use-auth.store.ts
+++ client/src/store/use-auth.store.ts
@@ -1,13 +1,14 @@
-import { Auth, Buyer, Seller } from "@/schemas";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import { Auth, Buyer, Seller } from "@/schemas";
 
 type States = {
-  //   authUser: Array<Auth> | [];
   authUser: Auth | null;
   buyer: Buyer | null;
+  role: "buyer" | "seller" | null;
   otherBuyer: Buyer | null;
   otherSeller: Seller | null;
-  //   seller: null;
+  seller: Seller | null;
 };
 
 type Actions = {
@@ -15,19 +16,28 @@ type Actions = {
   setBuyer: (buyer: Buyer) => void;
   setOtherBuyer: (buyer: Buyer) => void;
   setOtherSeller: (seller: Seller) => void;
+  setRole: (role: "buyer" | "seller" | null) => void;
 };
 
-export const useAuthStore = create<States & Actions>((set) => ({
-  authUser: null,
-  buyer: null,
-  otherBuyer: null,
-  otherSeller: null,
-  setAuthUser: (user: Auth) => set({ authUser: user }),
-  setBuyer: (buyer: Buyer) => set({ buyer: buyer }),
-  setOtherBuyer: (buyer: Buyer) => set({ otherBuyer: buyer }),
-  setOtherSeller: (seller: Seller) => set({ otherSeller: seller }),
-
-  // set(() => ({
-  //   authUser: user,
-  // })),
-}));
+export const useAuthStore = create<States & Actions>()(
+  persist(
+    (set, get) => ({
+      authUser: null,
+      buyer: null,
+      otherBuyer: null,
+      otherSeller: null,
+      role: null, // Initialize role state
+      seller: null,
+      setAuthUser: (user: Auth) => set({ authUser: user }),
+      setBuyer: (buyer: Buyer) => set({ buyer: buyer }),
+      setOtherBuyer: (buyer: Buyer) => set({ otherBuyer: buyer }),
+      setOtherSeller: (seller: Seller) => set({ otherSeller: seller }),
+      setRole: (role: "buyer" | "seller" | null) => set({ role: role }),
+    }),
+    {
+      name: "auth-storage", // A unique name for your storage
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({ role: state.role }), // Specify which part of the state to persist
+    }
+  )
+);
