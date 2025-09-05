8fe34aff9587bceb546a602ac27a576683391744
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-05 15:11:07 +0600

    style: update cotainer css

--- client/src/components/container.tsx
+++ client/src/components/container.tsx
@@ -4,7 +4,7 @@ interface Props {
 const Container = (props: Props) => {
   const { children } = props;
   return (
-    <div className="mx-auto box-border overflow-hidden px-[20px] md:max-w-[1200px] md:px-[40px] xl:max-w-[1450px] 2xl:max-w-[1736px] xl:px-[60px]">
+    <div className="mx-auto overflow-x-hidden  px-[20px] md:max-w-[1200px] md:px-[40px] xl:max-w-[1450px] 2xl:max-w-[1736px] xl:px-[60px]">
       {children}
     </div>
   );
