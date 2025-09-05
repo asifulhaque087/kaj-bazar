ae4c3ff5cc8f39c23dc12c905127bfa9aa2d6dfa
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-05 15:12:05 +0600

    feat: add container in homepage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,6 +6,7 @@ import Navigation from "@/components/Navigation";
 import { useState } from "react";
 import GigSlider from "@/components/gig-slider";
 import { useSearch } from "@/api/gigs";
+import Container from "@/components/container";
 
 export default function Home() {
   const [currentCategory, setCurrentCategory] =
@@ -23,28 +24,28 @@ export default function Home() {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className="px-[12px]">
-      {/* <Button>Click me</Button> */}
-
-      {/* Navigation */}
-
+    <Container>
+      {/* --- Navigation --- */}
       <div className="mt-[24px]">
         <Navigation />
       </div>
 
+      {/* --- Hero --- */}
       <div className="mt-[24px]">
         <Hero />
       </div>
 
+      {/* --- Category Tab --- */}
       <div className="mt-[24px]">
         <CategoryTab
           setActiveCategory={setCurrentCategory}
           activeCategory={currentCategory}
         />
       </div>
+      {/* --- Gig Slider --- */}
       <div className="mt-[24px]">
         <GigSlider gigs={data?.data || []} currentCategory={currentCategory} />
       </div>
-    </div>
+    </Container>
   );
 }
