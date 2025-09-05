d096290e89adcaa71fe461dd11c740432b422af8
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-05 15:12:32 +0600

    chore: add picsum image hostname

--- client/next.config.ts
+++ client/next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         hostname: "res.cloudinary.com",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "**",
+      },
     ],
   },
 };
