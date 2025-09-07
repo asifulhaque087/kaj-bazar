6459d50cff4b3332960eb1896814b9b272d2af05
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-07 18:05:17 +0600

    feat: update image input field header

--- client/src/components/image-field/header.tsx
+++ client/src/components/image-field/header.tsx
@@ -1,13 +1,22 @@
 "use client";
+
+import { CreateGigForm } from "@/schemas";
+import { FieldErrors } from "react-hook-form";
+
 interface Props {
   totalItems: number;
   removePhotos: () => void;
+  // errors: FieldErrors<CreateGigForm>;
 }
 const Header = (props: Props) => {
   const { totalItems, removePhotos } = props;
 
+  // const errorMessage = errors?.images?.message;
+
   return (
-    <div className="px-[3%] flex items-center border-b border-[#d3d3d3] justify-between h-[70px]">
+    <div
+      className={`px-[3%] flex items-center border-b justify-between h-[70px] `}
+    >
       {totalItems ? (
         <>
           <h1 className="text-[24px] text-[#353535] font-[700] capitalize">
