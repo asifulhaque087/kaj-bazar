b2113c7bd2d0f8f2a40aa930ec0ffbb72277897a
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-07 18:03:32 +0600

    feat: comment images field

--- client/src/forms/gig.form.ts
+++ client/src/forms/gig.form.ts
@@ -18,6 +18,51 @@ export const createGigDefaultForm = (
     subCategories: [], // No change here, but the type is now { title: string }[]
     expectedDelivery: "",
     coverImage: "",
+    // images: [],
+
+    // images: [
+    //   {
+    //     url: "https://final-gallery.netlify.app/images/image-1.webp",
+    //     orderId: 1,
+    //   },
+    //   {
+    //     url: "https://final-gallery.netlify.app/images/image-2.webp",
+    //     orderId: 2,
+    //   },
+    //   {
+    //     url: "https://final-gallery.netlify.app/images/image-4.webp",
+    //     orderId: 3,
+    //   },
+    //   // {
+    //   //   url: "https://final-gallery.netlify.app/images/image-5.webp",
+    //   //   orderId: 4,
+    //   // },
+    //   // {
+    //   //   url: "https://final-gallery.netlify.app/images/image-6.webp",
+    //   //   orderId: 5,
+    //   // },
+    //   // {
+    //   //   url: "https://final-gallery.netlify.app/images/image-7.webp",
+    //   //   orderId: 6,
+    //   // },
+    //   // {
+    //   //   url: "https://final-gallery.netlify.app/images/image-8.webp",
+    //   //   orderId: 7,
+    //   // },
+    //   // {
+    //   //   url: "https://final-gallery.netlify.app/images/image-9.webp",
+    //   //   orderId: 8,
+    //   // },
+    //   // {
+    //   //   url: "https://final-gallery.netlify.app/images/image-10.jpeg",
+    //   //   orderId: 9,
+    //   // },
+    //   // {
+    //   //   url: "https://final-gallery.netlify.app/images/image-11.jpeg",
+    //   //   orderId: 10,
+    //   // },
+    // ],
+
     price: 0,
 
     // optional
