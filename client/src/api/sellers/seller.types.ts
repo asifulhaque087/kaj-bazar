cc7c06af621eb3174d4ad994afd2acba1994a4a0
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-09 00:26:45 +0600

    feat: add UseUpdateSellerProps type

--- client/src/api/sellers/seller.types.ts
+++ client/src/api/sellers/seller.types.ts
@@ -1,4 +1,4 @@
-import { Auth, CreateSellerForm } from "@/schemas";
+import { Auth, CreateSellerForm, UpdateSellerForm } from "@/schemas";
 import { UseFormReset, UseFormSetError } from "react-hook-form";
 
 export interface UseCreateSellerProps {
@@ -6,8 +6,17 @@ export interface UseCreateSellerProps {
   reset: UseFormReset<CreateSellerForm>;
 }
 
+export interface UseUpdateSellerProps {
+  setError: UseFormSetError<UpdateSellerForm>;
+  reset: UseFormReset<UpdateSellerForm>;
+}
+
 export interface UseSellerByNameProps {
   username: string;
   isBuyer: boolean;
   authUser: Auth | null;
+}
+
+export interface UseSellerByIdProps {
+  sellerId: string;
 }
