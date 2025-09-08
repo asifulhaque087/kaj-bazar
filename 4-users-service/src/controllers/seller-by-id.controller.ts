df25099349402e0a6841c6cf29ae6c2b757a8f95
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-09 00:31:00 +0600

    feat: update seller by id controller

--- 4-users-service/src/controllers/seller-by-id.controller.ts
+++ 4-users-service/src/controllers/seller-by-id.controller.ts
@@ -1,7 +1,31 @@
+import { handleAsync } from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import { SellersTable } from "@src/drizzle/schemas";
+import { eq } from "drizzle-orm";
 import type { Request, Response } from "express";
 
-const getSellerById = (req: Request, res: Response) => {
-  return res.json({ m: "I am from get seller by id" });
+const getSellerById = async (req: Request, res: Response) => {
+  const { sellerId } = req.params;
+
+  // const [seller] = await handleAsync(
+  //   db.select().from(SellersTable).where(eq(SellersTable.id, sellerId!))
+  // );
+
+  const seller = await handleAsync(
+    db.query.SellersTable.findFirst({
+      where: eq(SellersTable.id, sellerId!),
+      with: {
+        languages: true,
+        skills: true,
+        experience: true,
+        education: true,
+        socialLinks: true,
+        certificates: true,
+      },
+    })
+  );
+
+  return res.json(seller);
 };
 
 export default getSellerById;
