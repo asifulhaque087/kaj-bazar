2933e0c931083ac08e180f4dd25802c417daba18
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-09 11:45:33 +0600

    feat: add constant

---
+++ client/src/constants.ts
@@ -0,0 +1,20 @@
+export const categories: string[] = [
+  "Graphics & Design",
+  "Digital Marketing",
+  "Writing & Translation",
+  "Video & Animation",
+  "Music & Audio",
+  "Programming & Tech",
+  "Photography",
+  "Data",
+  "Business",
+];
+export const expectedDelivery: string[] = [
+  "1 Day Delivery",
+  "2 Days Delivery",
+  "3 Days Delivery",
+  "4 Days Delivery",
+  "5 Days Delivery",
+];
+
+export const gigsLimit = "10";
