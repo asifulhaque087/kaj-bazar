d9c9f578c494b3f8b4a71eca91879ea5fbf5dc0f
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-12 21:41:56 +0600

    chore: move navigation into container

--- client/src/components/container.tsx
+++ client/src/components/container.tsx
@@ -1,3 +1,5 @@
+import Navigation from "@/components/Navigation";
+
 interface Props {
   children: React.ReactNode;
 }
@@ -5,6 +7,10 @@ const Container = (props: Props) => {
   const { children } = props;
   return (
     <div className="mx-auto overflow-x-hidden px-[16px] md:max-w-[1200px] md:px-[24px] xl:max-w-[1450px] 2xl:max-w-[1736px] xl:px-[60px]">
+      <header className="mt-[24px]">
+        <Navigation />
+      </header>
+
       {children}
     </div>
   );
