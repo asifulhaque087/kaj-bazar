2a9bbb2b07a5a9f8aaa38911ef94d8c484ef60e9
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-12 21:52:31 +0600

    feat: add buyer by id controller

---
+++ 4-users-service/src/controllers/buyer-by-id.controller.ts
@@ -0,0 +1,18 @@
+import { handleAsync } from "@fvoid/shared-lib";
+import { db } from "@src/drizzle/db";
+import { BuyersTable } from "@src/drizzle/schemas";
+import { eq } from "drizzle-orm";
+import type { Request, Response } from "express";
+
+const getBuyerById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const [buyer] = await handleAsync(
+    db.select().from(BuyersTable).where(eq(BuyersTable.id, id!))
+  );
+
+  return res.json(buyer);
+
+};
+
+export default getBuyerById;
