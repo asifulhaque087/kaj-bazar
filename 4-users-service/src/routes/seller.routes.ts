8f6113c50a2218b218abfc397c3883e5a2c83bc1
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-12 21:51:26 +0600

    feat: add current seller route

--- 4-users-service/src/routes/seller.routes.ts
+++ 4-users-service/src/routes/seller.routes.ts
@@ -10,11 +10,19 @@ import getSellerByName from "@src/controllers/seller-by-name.controller";
 import updateSeller from "@src/controllers/update-seller.controller";
 import { validateData } from "@src/middlewares/validation.middleware";
 import { createSellerSchema } from "@src/validations/create-serller.validation";
+import { verifyClientToken } from "@fvoid/shared-lib";
+import { config } from "@src/config";
+import getCurrentSeller from "@src/controllers/get-current-seller.controller";
 
 const sellerRouter = Router();
 
 sellerRouter.get("/username/:username", getSellerByName);
 sellerRouter.get("/random/:size", getRandomSellers);
+sellerRouter.get(
+  "/current-seller",
+  verifyClientToken(config.JWT_TOKEN),
+  getCurrentSeller
+);
 sellerRouter.get("/:sellerId", getSellerById);
 
 sellerRouter.post("/create", validateData(createSellerSchema), createSeller);
