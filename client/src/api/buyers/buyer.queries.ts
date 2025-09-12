729814f12cc62bfa26f8d51287822b8c57ca23f1
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-12 21:49:50 +0600

    feat: update current buyer logic

--- client/src/api/buyers/buyer.queries.ts
+++ client/src/api/buyers/buyer.queries.ts
@@ -1,20 +1,40 @@
-import { currentBuyer, getBuyerByName } from "@/api/buyers/buyer.service";
-import { UseBuyerByNameProps } from "@/api/buyers/buyer.types";
+import {
+  currentBuyer,
+  getBuyerById,
+  getBuyerByName,
+} from "@/api/buyers/buyer.service";
+import {
+  UseBuyerByIdProps,
+  UseBuyerByNameProps,
+} from "@/api/buyers/buyer.types";
 import { useQueryWithSideEffects } from "@/hooks/useQueryWithSideEffects";
 import { useAuthStore } from "@/store/use-auth.store";
 
-export const useCurrentBuyer = (authId: string | undefined) => {
+// export const useCurrentBuyer = (
+//   authId: string | undefined,
+//   role: "buyer" | "seller" | null
+// ) => {
+export const useCurrentBuyer = () => {
   // ** --- Store ---
-  const { setBuyer, setRole, role } = useAuthStore();
+  const {
+    setBuyer,
+    setRole,
+    role,
+    authUser,
+    initialRender,
+    setInitialRenderFalse,
+  } = useAuthStore();
 
   return useQueryWithSideEffects({
     queryKey: ["current_buyer"],
     queryFn: () => currentBuyer(),
-    enabled: !!authId && role !== "seller",
-
+    enabled: !!authUser?.id && role !== "seller" && initialRender,
+    staleTime: Infinity, // ✅ don’t refetch unnecessarily
+    gcTime: Infinity,
     onSuccess: (data) => {
       setBuyer(data);
       setRole("buyer");
+      setInitialRenderFalse();
     },
   });
 };
@@ -32,6 +52,23 @@ export const useBuyerByName = (props: UseBuyerByNameProps) => {
     enabled: !!authUser && !isBuyer,
     onSuccess: (data) => {
       setOtherBuyer(data);
+    },
+  });
+};
+
+export const useBuyerById = (props: UseBuyerByIdProps) => {
+  // ** --- Props ---
+  const { id } = props;
+
+  // ** --- Store ---
+  const { setBuyer, setRole } = useAuthStore();
+
+  return useQueryWithSideEffects({
+    queryKey: ["buyer", id],
+    queryFn: () => getBuyerById(id),
+    onSuccess: (data) => {
+      setBuyer(data);
+      setRole("buyer");
     },
   });
 };
