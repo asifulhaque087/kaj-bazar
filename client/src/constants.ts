f1b1e7b7d49f3d4efab24eb44737a3dbfa897d19
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-14 16:58:02 +0600

    feat: add footer data

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -18,3 +18,58 @@ export const expectedDelivery: string[] = [
 ];
 
 export const gigsLimit = "10";
+
+export const steps = [
+  {
+    heading: "Create a Gig",
+    subheading:
+      "Sign up for free, set up your Gig, and offer your work to our global audience.",
+  },
+  {
+    heading: "Deliver great work",
+    subheading:
+      "Get notified when you get an order and use our system to discuss details with customers.",
+  },
+  {
+    heading: "Get paid",
+    subheading:
+      "Get paid on time, every time. Payment is available for withdrawal as soon as it clears.",
+  },
+  {
+    heading: "Happy Client !",
+    subheading:
+      "Get paid on time, every time. Payment is available for withdrawal as soon as it clears.",
+  },
+];
+
+export const footerData = [
+
+  {
+    heading: "RESOURCES",
+    links: [
+      "Free eBooks",
+      "Development Tutorial",
+      "How to - Blog",
+      "Youtube Playlist",
+    ],
+  },
+
+  {
+    heading: "COMPANY",
+    links: ["About", "Features", "Works", "Career"],
+  },
+  {
+    heading: "HELP",
+    links: [
+      "Customer Support",
+      "Delivery Details",
+      "Terms & Conditions",
+      "Privacy Policy",
+    ],
+  },
+  {
+    heading: "FAQ",
+    links: ["Account", "Manage Deliveries", "Orders", "Payments"],
+  },
+];
+
