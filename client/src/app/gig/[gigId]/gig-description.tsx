004ccf22f46bec33a106410fd549bc82f13ec967
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-28 17:19:58 +0600

    feat: add gig description component

---
+++ client/src/app/gig/[gigId]/gig-description.tsx
@@ -0,0 +1,9 @@
+const GigDescription = () => {
+  return (
+    <div className="bg-[#FEFEFF] rounded-[4px] min-h-[316px] grid place-items-center">
+      This is gig description
+    </div>
+  );
+};
+
+export default GigDescription;
