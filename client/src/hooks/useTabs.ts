e9341de4b1e1bc83e68a9a8483c5b0c1d0585168
asifulhaque087 <asifulhaque087@gmail.com>
2025-09-29 19:08:47 +0600

    feat: update use tabs type

--- client/src/hooks/useTabs.ts
+++ client/src/hooks/useTabs.ts
@@ -1,10 +1,36 @@
+// import { useState } from "react";
+
+// interface Props<T extends object> {
+//   tabs: T[];
+// }
+
+// const useTabs = <T extends object>(props: Props<T>) => {
+//   const { tabs } = props;
+
+//   const [currentTabIndex, setCurrentTabIndex] = useState(0);
+
+//   const handleTabIndex = (i: number) => {
+//     if (i >= 0 && i < tabs.length) {
+//       setCurrentTabIndex(i);
+//     }
+//   };
+
+//   return {
+//     tabs,
+//     currentTabIndex,
+//     handleTabIndex,
+//   };
+// };
+
+// export default useTabs;
+
 import { useState } from "react";
 
-interface Props<T extends object> {
+interface Props<T extends object | string | number> {
   tabs: T[];
 }
 
-const useTabs = <T extends object>(props: Props<T>) => {
+const useTabs = <T extends object | string | number>(props: Props<T>) => {
   const { tabs } = props;
 
   const [currentTabIndex, setCurrentTabIndex] = useState(0);
