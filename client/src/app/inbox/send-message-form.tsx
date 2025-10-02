e076de5b1e1cd30543e2abfcd19b6d9f3b3c3e6a
asifulhaque087 <asifulhaque087@gmail.com>
2025-10-02 19:58:11 +0600

    feat: add functionality to send message form

--- client/src/app/inbox/send-message-form.tsx
+++ client/src/app/inbox/send-message-form.tsx
@@ -1,12 +1,48 @@
+import { useCreateMessage } from "@/api/chats";
+import { createMessageDefaultForm } from "@/forms";
+import { createMessageForm, CreateMessageForm } from "@/schemas";
+import { useChatStore } from "@/store/use-chat.store";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { LinkIcon, Send } from "lucide-react";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 const SendMessageForm = () => {
+  // ** --- Mutation ---
+  const { mutate: createMessage } = useCreateMessage();
+
+  // ** --- Store ---
+  const { selectedConversation, messageSenderUser, messageReceiverUser } =
+    useChatStore();
+
+  // ** --- Use form ---
+  const form = useForm<CreateMessageForm>({
+    resolver: zodResolver(createMessageForm),
+    defaultValues: createMessageDefaultForm(null, null, null),
+    mode: "onSubmit",
+  });
+
+  useEffect(() => {
+    if (messageSenderUser && messageReceiverUser && selectedConversation) {
+      const defaultForm = createMessageDefaultForm(
+        messageSenderUser,
+        messageReceiverUser,
+        selectedConversation
+      );
+      form.reset(defaultForm);
+    }
+  }, [messageSenderUser, messageReceiverUser, selectedConversation]);
+
+  // console.log("errors are ##### ", form.formState.errors)
+
   return (
     <form
       className="flex gap-x-[8px] px-[24px] h-[40px] w-full"
-      onSubmit={sendMessage}
+      // onSubmit={sendMessage}
+      onSubmit={form.handleSubmit(onSubmit)}
     >
       <input
+        {...form.register("body")}
         type="text"
         className="bg-[#FEFEFF] rounded-[8px] outline-none p-[12px] border border-[#E7E7E8] font-roboto text-[#3E3F47] text-sm  placeholder:font-roboto placeholder:text-[#6E6F75] placeholder:text-sm grow"
         placeholder="Type message ..."
@@ -29,6 +65,13 @@ const SendMessageForm = () => {
   function sendMessage(e: any) {
     e.preventDefault();
     console.log("send message data is ");
+  }
+
+  function onSubmit(data: CreateMessageForm) {
+    // console.log("send message data is ");
+    createMessage(data);
+    form.reset();
+    // form.reset({ ...data, body: "", file: "", hasOffer: false, offer: null });
   }
 };
 
