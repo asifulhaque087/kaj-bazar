6f10e6c932e3d52db3e4674a79887c6eb3e8de17
asifulhaque087 <asifulhaque087@gmail.com>
2025-10-03 00:13:09 +0600

    feat: show 2nd user in conversation list

--- client/src/app/inbox/conversation.tsx
+++ client/src/app/inbox/conversation.tsx
@@ -1,4 +1,5 @@
 import { Conversation as ConversationType } from "@/schemas";
+import { useAuthStore } from "@/store/use-auth.store";
 
 import { formatDistanceToNowStrict, parseISO } from "date-fns";
 import { useRouter } from "next/navigation";
@@ -9,6 +10,7 @@ interface Props {
 
 const Conversation = (props: Props) => {
   const { conversation } = props;
+  const { authUser } = useAuthStore();
 
   const router = useRouter();
 
@@ -17,21 +19,27 @@ const Conversation = (props: Props) => {
       className="flex items-center gap-x-[12px] border-b border-[#E7E7E8] py-[20px] px-[24px] cursor-pointer"
       onClick={() => {
         // setSelectedConversation(con);
-        // router.push(`/inbox/${conversation.id}?other=${findOtherUser(con)}`);
         router.push(`/inbox?conversation=${conversation.id}`);
       }}
     >
       <div className="w-[48px] h-[48px] rounded-full overflow-hidden">
         <img
           className="w-full h-full object-cover object-center"
-          src={conversation?.messages[0]?.senderPicture}
+          // src={conversation?.messages[0]?.senderPicture}
+          src={
+            conversation?.senderUsername === authUser?.username
+              ? conversation.receiverProfilePhoto
+              : conversation.senderProfilePhoto
+          }
           alt=""
         />
       </div>
       <div className="flex flex-col gap-y-1">
         <div className="flex items-center gap-x-4">
           <h6 className="font-roboto font-normal text-[16px] text-[#0E0F19]">
-            {conversation?.senderUsername}
+            {conversation?.senderUsername === authUser?.username
+              ? conversation.receiverUsername
+              : conversation.senderUsername}
           </h6>
           <span className="font-roboto font-normal text-xs text-[#3E3F47]">
             {conversation?.messages[0]?.createdAt &&
@@ -43,17 +51,15 @@ const Conversation = (props: Props) => {
         </div>
         <p className="font-roboto font-normal text-sm text-[#6E6F75]">
           {/* hello panze! can you help me building thi... */}
-          {conversation?.messages[0]?.body}
+          {/* {conversation?.messages[0]?.body} */}
+
+          {conversation?.messages[0]?.senderUsername === authUser?.username
+            ? `you: ${conversation?.messages[0]?.body}`
+            : conversation?.messages[0]?.body}
         </p>
       </div>
     </div>
   );
-
-  // function findOtherUser(con: Conversation): string {
-  //   return authUser?.username === con.receiverUsername
-  //     ? con.senderUsername
-  //     : con.receiverUsername;
-  // }
 };
 
 export default Conversation;
