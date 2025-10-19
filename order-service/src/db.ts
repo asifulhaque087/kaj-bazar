ebda59695029af08db9d5fd14ee9739b6a63dd26
asifulhaque087 <asifulhaque087@gmail.com>
2025-10-20 01:04:06 +0600

    feat: rename order service

--- order-service/src/db.ts
+++ order-service/src/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@src/schemas";
 import { config } from "@src/config";
 
-import { Pool } from "pg";
+import { Pool } from "node_modules/@types/pg";
 
 export const pool = new Pool({
   connectionString: config.DATABASE_URL,
