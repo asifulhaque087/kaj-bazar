0ad8b52d10eef4dc954a021fc0afbf562de0daec
asifulhaque087 <asifulhaque087@gmail.com>
2025-10-25 00:16:13 +0600

    fix: use env for base endpoint

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,4 @@
 export const config = {
-  // API_GATEWAY_URL: process.env.API_GATEWAY_URL || "http://gateway:4000",
-  API_GATEWAY_URL: "http://localhost:4000/api/v1/gateway",
+  API_GATEWAY_URL: process.env.NEXT_PUBLIC_BASE_ENDPOINT || "",
   CLIENT_URL: process.env.CLIENT_URL || "",
 };
