a3c697e91295ed2b495846152ccff93fcaf35830
asifulhaque087 <asifulhaque087@gmail.com>
2025-10-25 00:45:45 +0600

    chore: check with hard code value

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,5 @@
 export const config = {
-  API_GATEWAY_URL: process.env.NEXT_PUBLIC_BASE_ENDPOINT || "",
+  // API_GATEWAY_URL: process.env.NEXT_PUBLIC_BASE_ENDPOINT || "",
+  API_GATEWAY_URL: "https://kajbazar-api.asif-haque.com/api/v1/gateway",
   CLIENT_URL: process.env.CLIENT_URL || "",
 };
