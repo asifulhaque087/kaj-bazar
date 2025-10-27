ed1f99d43f7c8a4af36ea7d1489b9ead53c71b4d
asifulhaque087 <asifulhaque087@gmail.com>
2025-10-27 16:11:08 +0600

    feat: add logout functionality

--- gateway-service/src/controllers/auth/logout.controller.ts
+++ gateway-service/src/controllers/auth/logout.controller.ts
@@ -1,8 +1,8 @@
 import type { Request, Response } from "express";
-import { type AxiosResponse, AxiosError } from "axios"; // Import AxiosResponse and AxiosError
 
 const logout = async (req: Request, res: Response) => {
-  return res.json({ m: "This is signout" });
+  req.session = null;
+  return res.json({ message: "Logout successful", user: {} });
 };
 
 export default logout;
